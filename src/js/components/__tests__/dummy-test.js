0ff5f63d345cba5852309ad953959a6b5e562255
romaintailhurat <romain.tailhurat@gmail.com>
2015-03-04 21:41:00 +0100

    A dummy test file proving that jest infra is working

---
+++ src/js/components/__tests__/dummy-test.js
@@ -0,0 +1,12 @@
+/*
+Dummy test to validate Jest use =)
+*/
+describe('dummy try something', function() {
+  it('should fail miserably', function() {
+    expect(true).toBe(false);
+  });
+
+  it('should be victorious !', function() {
+    expect(true).toBe(true);
+  });
+});
