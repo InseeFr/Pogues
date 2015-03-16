cffa588a8a881f6d61c845931a4f4a108e2920e7
romaintailhurat <romain.tailhurat@gmail.com>
2015-03-16 18:53:59 +0100

    Questionnaire outlook now display questions under modules

--- src/js/components/questionnaire-outlook.js
+++ src/js/components/questionnaire-outlook.js
@@ -24,12 +24,31 @@ var QuestionnaireOutlook = React.createClass({
 		QuestionnaireStore.removeChangeListener(this._onChange);
 	},
 	render: function() {
+		// FIXME couldn't we be cleaner ???
 		return (
 			<div className="col-md-3">
 				<nav className="bs-docs-sidebar hidden-print hidden-xs hidden-sm affix-top">
 					<ul className="nav bs-docs-sidenav">
 						{this.state.questionnaire.modules.map(function(module, index) {
-							return (<li key={index}>{module.name}</li>)
+							if( module.questions === undefined || module.questions.length == 0 ) {
+								return (
+									<li key={index}>{ module.name }</li>
+								);
+							} else {
+								return (
+									<li key={index}>
+										{ module.name }
+										<ul>
+										{ module.questions.map(function(question, index) {
+												return(
+													<li>{question.name}</li>
+													)
+										}) }
+										</ul>
+									</li>
+									)
+							}
+
 						})}
 					</ul>
 				</nav>
