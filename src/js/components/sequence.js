cd38356620c5cb513eb5fc6be649b3dc74e30d68
Franck <franck.cotton@insee.fr>
2015-03-30 20:48:08 +0200

    Workaround for mysterious bug on Component component

--- src/js/components/sequence.js
+++ src/js/components/sequence.js
@@ -1,6 +1,8 @@
 /** @jsx React.DOM */
 var React = require('react');
 var Component = require('../components/component');
+var Question = require('../components/question');
+var SequenceModel = require('../models/Sequence');
 var Edit = require('../components/edit');
 
 var Sequence = React.createClass({
@@ -13,7 +15,9 @@ var Sequence = React.createClass({
 					<div className="col-md-10">
 					 <h2 >{this.props.sequence.name}</h2>
 					  {this.props.sequence.children.map(function(child, index) {
-						  return (<div><Component component={child}/></div>);
+						  //return (<Component component={child}/>);
+						  if (child instanceof SequenceModel) return(<Sequence key={index} sequence={child}/>);
+						  else return(<Question key={index} question={child}/>);
 					  })}
 					</div>
 					<Edit />
