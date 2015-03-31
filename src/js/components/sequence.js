76b5efe780ebd57151e0d3194726264f552e4386
Franck <franck.cotton@insee.fr>
2015-03-31 16:23:53 +0200

    Sequence rendering improved (the Tag trick)

--- src/js/components/sequence.js
+++ src/js/components/sequence.js
@@ -7,24 +7,24 @@ var Edit = require('../components/edit');
 
 var Sequence = React.createClass({
 
-	render: function() {
-		console.log('Sequence rendering with props', this.props);
+  render: function() {
+    console.log('Sequence rendering with props', this.props);
 
-			return (
-				<div className="row component">
-					<div className="col-md-10">
-					 <h2 >{this.props.sequence.name}</h2>
-					  {this.props.sequence.children.map(function(child, index) {
-						  //return (<Component component={child}/>);
-						  if (child instanceof SequenceModel) return(<Sequence key={index} sequence={child}/>);
-						  else return(<Question key={index} question={child}/>);
-					  })}
-					</div>
-					<Edit />
-				</div>
-			);
-
-	}
+    var Tag = 'h' + this.props.sequence.depth;
+    return (
+      <div className="row component">
+        <div className="col-md-10">
+         <Tag>{this.props.sequence.name}</Tag>
+          {this.props.sequence.children.map(function(child, index) {
+            //return (<Component component={child}/>);
+            if (child instanceof SequenceModel) return(<Sequence key={index} sequence={child}/>);
+            else return(<Question key={index} question={child}/>);
+          })}
+        </div>
+        <Edit />
+      </div>
+    );
+  }
 });
 
 module.exports = Sequence;
