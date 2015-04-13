3e76558357dcdc7911724cc160f0820f0a1fc770
romaintailhurat <romain.tailhurat@gmail.com>
2015-04-13 18:42:01 +0200

    Save questionnaire features on, from button to REST call

--- src/js/components/menu.js
+++ src/js/components/menu.js
@@ -1,5 +1,8 @@
 var React = require('react');
 var locale = require('../stores/dictionary-store').getDictionary();
+var PoguesActions = require('../actions/pogues-actions');
+var ViewTypes = require('../constants/pogues-constants').ViewTypes;
+var QuestionnaireStore = require('../stores/questionnaire-store');
 
 var Menu = React.createClass({
 
@@ -16,10 +19,25 @@ var Menu = React.createClass({
     _filter: function() {
         this.props.handleFilter(this.state.filter);
     },
+    _clickToSave: function() {
+      // Mock Questionnaire FIXME
+      console.log('click on save questionnaire button');
+      PoguesActions.saveQuestionnaire(QuestionnaireStore.getQuestionnaire());
+    },
 
 	render: function() {
         // FIXME: add bootstrap javascript dependency for toggling behavior
         // TODO: handle connected user properly
+    var isQuestionnaireView = this.props.view === ViewTypes.QUESTIONNAIRE;
+    var saveButton = null;
+    if(isQuestionnaireView) {
+      saveButton =
+      <div className="nav navbar-nav navbar-left">
+        <form className="navbar-form navbar-right">
+          <button className="btn btn-primary" onClick={this._clickToSave}>Sauvegarder</button>
+        </form>
+      </div>
+    }
 		return(
             <nav className="navbar navbar-default">
                 <div className="container-fluid">
@@ -29,6 +47,7 @@ var Menu = React.createClass({
                         </button>
                         <a className="navbar-brand" href=".">Pogues</a>
                     </div>
+
                     <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                         <ul className="nav navbar-nav">
                             <li><em className="navbar-text">{this.props.title}</em></li>
@@ -40,14 +59,15 @@ var Menu = React.createClass({
                             </div>
                             <button className="btn btn-default" onClick={this._filter}>{locale.search}</button>
                         </div>
-                        <ul className="nav navbar-nav navbar-right">
-                        <li className="dropdown">
-                          <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Me, myself and I <span className="caret"></span></a>
-                          <ul className="dropdown-menu" role="menu">
-                            <li><a href="#">Disconnect</a></li>
+                        {saveButton}
+                          <ul className="nav navbar-nav navbar-right">
+                          <li className="dropdown">
+                            <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Me, myself and I <span className="caret"></span></a>
+                            <ul className="dropdown-menu" role="menu">
+                              <li><a href="#">Disconnect</a></li>
+                            </ul>
+                          </li>
                           </ul>
-                        </li>
-                        </ul>
                     </div>
                 </div>
             </nav>
