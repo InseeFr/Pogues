d2505f3c8091441ce9bc47dec7d23cfaf7a74b89
Franck <franck.cotton@gmail.com>
2015-04-16 17:57:56 +0200

    New DataDatatype type

--- src/js/models/DateDatatype.js
+++ src/js/models/DateDatatype.js
@@ -7,9 +7,49 @@ class DateDatatypeModel extends DatatypeModel {
   constructor(object) {
     super(object);
     if (object) {
-     } else {
+      this._minimum = object._minimum;
+      this._maximum = object._maximum;
+      this._format = object._format;
+    } else {
+      this._minimum = new Date();
+      this._maximum = new Date();
+      this._format = '';
     }
   }
+
+  get minimum() {
+    return this._minimum;
+  }
+
+  get maximum() {
+    return this._maximum;
+  }
+
+  get format() {
+    return this._format;
+  }
+
+  set minimum(minimum) {
+    if (!(minimum instanceof Date) {
+      throw new Error('The parameter must be a Date object');
+    }
+    this._minimum = minimum;
+  }
+
+  set maximum(maximum) {
+    if (!(maximum instanceof Date)) {
+      throw new Error('The parameter must be a Date object');
+    }
+    this._maximum = maximum;
+  }
+
+  set format(format) {
+    if (typeof typeName !== 'string') {
+      throw new Error('The parameter must be a string');
+    }
+    this._format = format;
+  }
+
 }
 
 export default DateDatatypeModel;
