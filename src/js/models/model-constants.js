aa1f897927ae6b2ce645ed4215c63ce56343f907
Franck <franck.cotton@gmail.com>
2015-04-16 13:00:27 +0200

    Refactoring of constants management in the model, part 2

---
+++ src/js/models/model-constants.js
@@ -0,0 +1,12 @@
+module.exports = {
+
+  SequenceModel: {
+    GENERIC_NAMES: ['QUESTIONNAIRE', 'MODULE', 'PARAGRAPH', 'SEQUENCE']
+  },
+  DatatypeModel: {
+    DATATYPE_TYPES: ['DATE', 'NUMERIC', 'TEXT']
+  },
+  DeclarationModel: {
+    DECLARATION_TYPES: ['INSTRUCTION', 'COMMENT', 'HELP']
+  }
+};
