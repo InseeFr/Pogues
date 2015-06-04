82420a1f71c02661dceaa4052252ae38fc5e94cf
Jean-Baptiste Rudant <boogaloojb@gmail.com>
2015-06-05 00:16:15 +0200

    Create a target component to choose a target for a goTo

---
+++ src/js/components/target.js
@@ -0,0 +1,49 @@
+var React = require('react')
+var ComponentPicker = require('./ComponentPicker')
+var ComponentModel = require('../models/Component')
+
+var Target = React.createClass({
+  propTypes: {
+    text: React.PropTypes.string.isRequired,
+    handleChange: React.PropTypes.func.isRequired,
+    initialTarget: React.PropTypes.instanceOf(ComponentModel),
+    candidates: React.PropTypes.arrayOf(
+      React.PropTypes.instanceOf(ComponentModel)).isRequired
+  },
+  getInitialState: function() {
+    return {
+      target: this.props.initialTarget
+    }
+  },
+  _handleChange: function(target) {
+    this.props.handleChange(target)
+    this.setState({
+      target: target
+    })
+  },
+	render: function() {
+
+    // TODO remove target.id from cmpntName (debugging purposes)
+    var cmpntName = this.state.target ?
+                      this.state.target.label + '(' + this.state.target.id + ')' :
+                      ""
+		return (
+      <div className="form-group has-success has-feedback">
+  			<label className="col-sm-4 control-label">{this.props.text}</label>
+      		<div className="col-sm-4">
+        	<ComponentPicker
+            initialValue={this.props.initialValue}
+            candidates={this.props.candidates}
+        		handleChange={this._handleChange}/>
+        	<span className="glyphicon glyphicon-ok form-control-feedback" aria-hidden="true"></span>
+      	</div>
+      	<div className="col-sm-4">
+        	<input enabled={false} className="form-control"
+            value={cmpntName}/>
+      	</div>
+      </div>
+		);
+	}
+});
+
+module.exports = Target
