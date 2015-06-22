a75c7891c5334639e7ad84520c2ddc1f60beeccc
Jean-Baptiste Rudant <boogaloojb@gmail.com>
2015-06-22 14:35:35 +0200

    Mock codelists

---
+++ src/js/utils/mockcodelist.js
@@ -0,0 +1,23 @@
+var CodeListModel = require('../models/CodeList')
+
+var CodelistRepository = {
+  'cl_propsal': new CodeListModel({
+    id: 'cl_propsal',
+    name: 'cl_propsal',
+    label: 'Proportion de salaire',
+    codes: [
+      {value: 0, label: 'La moitié ou plus'},
+      {value: 1, label: 'Moins de la moitié'}
+    ]}),
+  'cl_sexe': new CodeListModel({
+      id: 'cl_sexe',
+      name: 'cl_sexe',
+      label: 'Sexe',
+      codes: [
+        {value: 0, label: 'Homme'},
+        {value: 1, label: 'Femme'}
+      ]
+    })
+  }
+
+modules.exports = CodelistRepository;
