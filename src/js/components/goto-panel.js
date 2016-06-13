9ac326f38606dbc726c997fced1f0e84a0f9bb12
Jean-Baptiste RUDANT <jb@noknot.fr>
2016-06-13 08:49:33 +0200

    Refactor panel components

---
+++ src/js/components/goto-panel.js
@@ -0,0 +1,109 @@
+import GenericPanel from './generic-panel'
+import GoTo from './go-to'
+import { connect } from 'react-redux'
+import React, { PropTypes } from 'react'
+import { createGoTo, removeGoTo, editGoTo } from '../actions/goTo'
+import { GOTO_CONSISTENCY } from '../constants/pogues-constants'
+const { AFTER, BEFORE, NON_EXISTING } = GOTO_CONSISTENCY
+
+/**
+ * GoToPanel Component
+ *
+ * Renders all the GoTos associated to a question or a sequence
+ *
+ */
+function GoToPanel(
+    { cmpntId, detailedGoTos, createGoTo, removeGoTo, editGoTo,
+      before, after, labelToId, idToLabel, idToRank, locale }) {
+
+  const changeTargetByName = (trueOrFalse, goToId) => name => {
+    const id = labelToId[name]
+    editGoTo(goToId, {
+      [trueOrFalse ? 'ifTrue' : 'ifFalse']: id || name,
+      [trueOrFalse ? 'ifTrueIsALabel' : 'ifFalseIsALabel']: !(id)
+    })
+  }
+  // Event if ifTrueIsALabel is set to true, it doesn't mean that the target
+  // does not exist : it might not exist when the goto was defined, but
+  // having been created later.
+
+  let goToEls = detailedGoTos.length > 0 ?
+  	detailedGoTos.map(({ id, description, expression, ifTrue, ifFalse,
+          ifTrueIsALabel, ifFalseIsALabel }) => {
+      const cmpntRank = idToRank[cmpntId]
+
+      const ifTrueId = ifTrueIsALabel ? labelToId[ifTrue] : ifTrue
+      const ifTrueLabel = ifTrueIsALabel ? ifTrue : idToLabel[ifTrue]
+      const ifTrueStatus = ifTrueId ?
+        (idToRank[ifTrueId] > cmpntRank ? AFTER : BEFORE) :
+        NON_EXISTING
+      
+      const ifFalseId = ifFalseIsALabel ? labelToId[ifFalse] : ifFalse
+      const ifFalseLabel = ifFalseIsALabel ? ifFalse : idToLabel[ifFalse]
+      const ifFalseStatus = ifFalseId ?
+        (idToRank[ifFalseId] > cmpntRank ? AFTER : BEFORE) :
+        NON_EXISTING
+
+      return <GoTo key={id} id={id}
+        before={before} after={after}
+        expression={expression} description={description}
+        ifTrueLabel={ifTrueLabel}
+        ifFalseLabel={ifFalseLabel}
+        ifTrueStatus={ifTrueStatus}
+        ifFalseStatus={ifFalseStatus}
+        remove={() => removeGoTo(id, cmpntId)}
+        locale={locale}
+        edit={update => editGoTo(id, update)}
+        changeTargetTrue={changeTargetByName(true, id)}
+        changeTargetFalse={changeTargetByName(false, id)} />
+    }) :
+    <span>No goTo yet</span>
+
+    return (
+      <div>
+        <datalist id="candidates">
+          {after.map(({ id, label }) => <option key={id} value={label} />)}
+        </datalist>
+        <GenericPanel add={() => createGoTo(cmpntId)}
+          children={goToEls} 
+    	    localeAdd={locale.defineGoTo} localeTitle={locale.goTo}  />
+      </div>
+    )
+  } 
+
+GoToPanel.propTypes = {
+  detailedGoTos: PropTypes.array.isRequired,
+  createGoTo: PropTypes.func.isRequired,
+  removeGoTo: PropTypes.func.isRequired,
+  editGoTo: PropTypes.func.isRequired,
+  before: PropTypes.array.isRequired,
+  after: PropTypes.array.isRequired,
+  locale: PropTypes.object.isRequired,
+  labelToId: PropTypes.object.isRequired,
+  idToLabel: PropTypes.object.isRequired,
+  idToRank: PropTypes.object.isRequired
+}
+
+const mapStateToProps = (state, { goTos, structure, cmpntId }) => {
+  const { flat, idToRank, labelToId, idToLabel } = structure
+  return {
+    detailedGoTos: goTos.map(id => state.goToById[id]),
+    before: flat.slice(0, idToRank[cmpntId]),
+    after: flat.slice(idToRank[cmpntId]+1),
+    labelToId,
+    idToLabel,
+    idToRank,
+    locale: state.locale
+  }
+}
+
+
+const mapDispatchToProps = {
+  createGoTo: createGoTo,
+  editGoTo: editGoTo,
+  removeGoTo: removeGoTo
+}
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(GoToPanel)
+
---
+++ src/js/components/response-panel.js
@@ -0,0 +1,54 @@
+import React, { PropTypes } from 'react'
+import GenericPanel from './generic-panel'
+import Response from './response';
+import Logger from '../logger/logger';
+import { connect } from 'react-redux'
+import { 
+  createResponse, removeResponse, editResponse, changeDatatypeName,
+  changeDatatypeParam, editResponseChooseCodeList
+} from '../actions/response'
+
+var logger = new Logger('ResponsePanel', 'Components');
+
+function ResponsePanel({ cmpntId, detailedResponses, removeResponse,
+    createResponse,  editResponse, editResponseChooseCodeList,
+    changeDatatypeName, changeDatatypeParam,
+    locale }) {
+
+  const rspnsEls = detailedResponses.map(
+    ({ id, simple, codeListReference, mandatory, datatype }) =>
+    <Response key={id} id={id} simple={simple} mandatory={mandatory}
+      datatype={datatype} codeListReference={codeListReference}
+      remove={() => removeResponse(id, cmpntId) }
+      edit={update => editResponse(id, update)}
+      editChooseCodeList={clId => editResponseChooseCodeList(id, clId)}
+      changeDatatypeName={typeName => changeDatatypeName(id, typeName)}
+      changeDatatypeParam={update => changeDatatypeParam(id, update)}
+      locale={locale} /> )
+
+  return <GenericPanel add={() => createResponse(cmpntId)} children={rspnsEls} 
+    localeAdd={locale.addResponse} localeTitle={locale.responsesEdition}  />
+}
+
+ResponsePanel.propTypes = {
+  detailedResponses: PropTypes.array.isRequired,
+  removeResponse: PropTypes.func.isRequired,
+  createResponse: PropTypes.func.isRequired,
+  editResponse: PropTypes.func.isRequired,
+  editResponseChooseCodeList: PropTypes.func.isRequired,
+  locale: PropTypes.object.isRequired
+}
+
+const mapStateToProps = (state, { cmpntId, responses }) => ({
+  detailedResponses: responses.map(id => state.responseById[id])
+})
+
+const mapDispatchToProps = {
+  createResponse,
+  removeResponse,
+  editResponse,
+  editResponseChooseCodeList,
+  changeDatatypeName,
+  changeDatatypeParam
+}
+export default connect(mapStateToProps, mapDispatchToProps)(ResponsePanel)
