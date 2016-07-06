8b7d596e54f0c1eb6bd954a87bcc1506d9813f46
Jean-Baptiste RUDANT <boogaloojb@gmail.com>
2016-07-06 10:55:02 +0200

    Clean and update config files

--- src/js/config/config.js
+++ src/js/config/config.js
@@ -2,34 +2,35 @@
 //urls
 var config = {
   dev :  true,
-  allowRemovalOfQuestionnaire: true,
-  baseURL : 'http://localhost:4000',
-  poguesPath : '',
-  persistPath : '',
-  stromaePath : '/stromae/publisher', 
+  allowRemovalOfQuestionnaire: false,
+  baseURL : 'http://s90datalift.ad.insee.intra:9050',
+  poguesPath : '/exist/pogues',
+  persistPath : '/exist/restxq',
+  stromaePath : '/exist/stromae/publisher',
   log : {
     level : 'DEBUG',
     activeNamespaces : ['Components', 'Actions', 'Stores', 'Utils', 'Models']
   },
   codeLists: {
-    repoURLSpecs: 'http://localhost:4000/repo/specs',
-    repoURLCList: 'http://localhost:4000/repo/clist'
+    //repoURLCList not implemented
+    repoURLSpecs: makeRepoURLSpecs(),
   }
 }
 
-//TODO see if this all the parameters should be exposed and modifiable
-//from the config editor
+//TODO see if this all the parameters should be exposed and modifiabl//from the
+// config editor
 function makeRepoURLSpecs() {
-  const repoURL = 'http://dvrmessnclas01.ad.insee.intra:80s80'
+  const repoURL = 'http://dvrmessnclas01.ad.insee.intra:8080'
   const query = `
-    // PREFIX skos:<http://www.w3.org/2004/02/skos/core#>
-    // PREFIX xkos:<http://rdf-vocabulary.ddialliance.org/xkos#>
+    PREFIX skos:<http://www.w3.org/2004/02/skos/core#>
+    PREFIX xkos:<http://rdf-vocabulary.ddialliance.org/xkos#>
 
-    // SELECT ?niveau ?label ?retrievalQuery WHERE {
-    //   ?niveau a xkos:ClassificationLevel ; skos:prefLabel ?label .
-    //   BIND(CONCAT("PREFIX skos:<http://www.w3.org/2004/02/skos/core#> PREFIX xkos:<http://rdf-vocabulary.ddialliance.org/xkos#> SELECT ?code ?intitule  WHERE {<", STR(?niveau), "> skos:member ?poste . ?poste skos:notation ?code ; skos:prefLabel ?intitule .} ORDER BY ?code") AS ?retrievalQuery)
-    // }`
+    SELECT ?niveau ?label ?retrievalQuery WHERE {
+       ?niveau a xkos:ClassificationLevel ; skos:prefLabel ?label .
+       BIND(CONCAT("PREFIX skos:<http://www.w3.org/2004/02/skos/core#> PREFIX xkos:<http://rdf-vocabulary.ddialliance.org/xkos#> SELECT ?code ?intitule  WHERE {<", STR(?niveau), "> skos:member ?poste . ?poste skos:notation ?code ; skos:prefLabel ?intitule .} ORDER BY ?code") AS ?retrievalQuery)
+     }`
   return `${repoURL}/sparql?query=${encodeURIComponent(query)}`
 }
+
 
 export default config
--- src/js/config/config.local.js
+++ src/js/config/config.local.js
@@ -6,7 +6,7 @@ var config = {
   baseURL : 'http://localhost:4000',
   poguesPath : '',
   persistPath : '',
-  stromaePath : '/stromae/publisher', 
+  stromaePath : '/stromae/publisher',
   log : {
     level : 'DEBUG',
     activeNamespaces : ['Components', 'Actions', 'Stores', 'Utils', 'Models']
@@ -15,21 +15,6 @@ var config = {
     repoURLSpecs: 'http://localhost:4000/repo/specs',
     repoURLCList: 'http://localhost:4000/repo/clist'
   }
-}
-
-//TODO see if this all the parameters should be exposed and modifiable
-//from the config editor
-function makeRepoURLSpecs() {
-  const repoURL = 'http://dvrmessnclas01.ad.insee.intra:80s80'
-  const query = `
-    // PREFIX skos:<http://www.w3.org/2004/02/skos/core#>
-    // PREFIX xkos:<http://rdf-vocabulary.ddialliance.org/xkos#>
-
-    // SELECT ?niveau ?label ?retrievalQuery WHERE {
-    //   ?niveau a xkos:ClassificationLevel ; skos:prefLabel ?label .
-    //   BIND(CONCAT("PREFIX skos:<http://www.w3.org/2004/02/skos/core#> PREFIX xkos:<http://rdf-vocabulary.ddialliance.org/xkos#> SELECT ?code ?intitule  WHERE {<", STR(?niveau), "> skos:member ?poste . ?poste skos:notation ?code ; skos:prefLabel ?intitule .} ORDER BY ?code") AS ?retrievalQuery)
-    // }`
-  return `${repoURL}/sparql?query=${encodeURIComponent(query)}`
 }
 
 export default config
