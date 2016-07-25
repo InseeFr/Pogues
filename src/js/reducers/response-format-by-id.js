InseeFr/Pogues

commit e1654236297a63a7e0307b8fc1da05d208a284c6

Fix use of local string properites instead of constants
The code was working, but only because strings constants have a value equal to the constant name. We need to use dynamic properties based on constants instead.

src/js/reducers/response-format-by-id.js
@@ -1,9 +1,11 @@
 import {
   SWITCH_FORMAT, CHANGE_DATATYPE_PARAM, UPDATE_SINGLE
+  SWITCH_FORMAT, CHANGE_DATATYPE_PARAM, CHANGE_DATATYPE_NAME,
+  UPDATE_SINGLE, NEW_CODE_LIST_SINGLE,
 } from '../actions/response-format'
-import {
-  LOAD_QUESTIONNAIRE_SUCCESS
-} from '../actions/questionnaire'
+
+import { LOAD_QUESTIONNAIRE_SUCCESS } from '../actions/questionnaire'
+import { CREATE_COMPONENT } from '../actions/component'
 
 import { RESPONSE_FORMAT, COMPONENT_TYPE } from '../constants/pogues-constants'
 import { emptyTextDatatype, emptyDatatypeFactory } from './datatype-utils'
@@ -73,6 +75,13 @@ const actionsHndlrs = {
   UPDATE_SINGLE: fromFormatHndlr(updateSingle),
   NEW_CODE_LIST_SINGLE: fromFormatHndlr(newCodeListSingle),
   LOAD_QUESTIONNAIRE_SUCCESS: loadQuestionnaireSuccess
+  [CREATE_COMPONENT]: createComponent,
+  [SWITCH_FORMAT]: switchFormat,
+  [CHANGE_DATATYPE_PARAM]: fromFormatHndlr(changeDatatypeParam),
+  [CHANGE_DATATYPE_NAME]: fromFormatHndlr(changeDatatypeName),
+  [UPDATE_SINGLE]: fromFormatHndlr(updateSingle),
+  [NEW_CODE_LIST_SINGLE]: fromFormatHndlr(newCodeListSingle),
+  [LOAD_QUESTIONNAIRE_SUCCESS]: loadQuestionnaireSuccess
 }
 
 export default function (state={}, action) {

commit 7f929662e539777f9e0cd46ed234f5e06eeb7a9e

Fix missing semicolon and duplicate declaration
Issues introduced by e1654236297a63a7e0307b8fc1da05d208a284c6

src/js/reducers/response-format-by-id.js
@@ -1,5 +1,4 @@
 import {
-  SWITCH_FORMAT, CHANGE_DATATYPE_PARAM, UPDATE_SINGLE
   SWITCH_FORMAT, CHANGE_DATATYPE_PARAM, CHANGE_DATATYPE_NAME,
   UPDATE_SINGLE, NEW_CODE_LIST_SINGLE,
 } from '../actions/response-format'
@@ -74,7 +73,7 @@ const actionsHndlrs = {
   CHANGE_DATATYPE_NAME: fromFormatHndlr(changeDatatypeName),
   UPDATE_SINGLE: fromFormatHndlr(updateSingle),
   NEW_CODE_LIST_SINGLE: fromFormatHndlr(newCodeListSingle),
-  LOAD_QUESTIONNAIRE_SUCCESS: loadQuestionnaireSuccess
+  LOAD_QUESTIONNAIRE_SUCCESS: loadQuestionnaireSuccess,
   [CREATE_COMPONENT]: createComponent,
   [SWITCH_FORMAT]: switchFormat,
   [CHANGE_DATATYPE_PARAM]: fromFormatHndlr(changeDatatypeParam),