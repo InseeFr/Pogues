InseeFr/Pogues

commit fd3e232a462fe5323a6dec21c40ae6b399d02595

Add support for single response

src/js/reducers/code-list-by-questionnaire.js
@@ -8,20 +8,23 @@
 //TODO implement remove code list (with integrity controls to check if the
 //code list is not used by any response)
 import { CREATE_CODE_LIST } from '../actions/code-list'
-import { CREATE_QUESTIONNAIRE, LOAD_QUESTIONNAIRE_SUCCESS } from '../actions/questionnaire'
+import { NEW_CODE_LIST_SINGLE } from '../actions/response-format'
+import {
+  CREATE_QUESTIONNAIRE, LOAD_QUESTIONNAIRE_SUCCESS
+} from '../actions/questionnaire'
 
 export default function (state={}, action) {
   if (action.type === CREATE_QUESTIONNAIRE) {
     return {
       ...state,
-      [action.payload.id]: []
+      [action.payload.qrId]: []
     }
   }
-  else if (action.type === CREATE_CODE_LIST) {
-    const { id, qrId } = action.payload
+  else if (action.type === NEW_CODE_LIST_SINGLE) {
+    const { createdClId, qrId } = action.payload
     return {
      ...state,
-      [qrId]: [...state[qrId], id]
+      [qrId]: [...state[qrId], createdClId]
     }
   }
   else if (action.type === LOAD_QUESTIONNAIRE_SUCCESS) {

src/js/actions/response-format.js
@@ -3,6 +3,10 @@ export const CHANGE_DATATYPE_NAME = 'CHANGE_DATATYPE_NAME'
 export const CHANGE_DATATYPE_PARAM = 'CHANGE_DATATYPE_PARAM'
 export const EDIT_RESPONSE_CHOOSE_CODE_LIST = 'EDIT_RESPONSE_CHOOSE_CODE_LIST'
 export const SWITCH_FORMAT = 'SWITCH_FORMAT'
+export const UPDATE_SINGLE = 'UPDATE_SINGLE'
+export const NEW_CODE_LIST_SINGLE = 'NEW_CODE_LIST_SINGLE'
+
+import { uuid } from '../utils/data-utils'
 
 export const switchFormat = (id, type) => ({
   type: SWITCH_FORMAT,
@@ -12,12 +16,20 @@ export const switchFormat = (id, type) => ({
   }
 })
 
-
-export const editResponse = (id, update) => ({
-  type: EDIT_RESPONSE,
+export const updateSingle = (id, update) => ({
+  type: UPDATE_SINGLE,
   payload: {
     id,
     update
+  }
+})
+
+export const newCodeListSingle = (id, qrId) => ({
+  type: NEW_CODE_LIST_SINGLE,
+  payload: {
+    id,
+    qrId,
+    createdClId: uuid()
   }
 })
 

src/js/components/response-format.js
@@ -2,11 +2,12 @@ import React, { PropTypes } from 'react';
 import ResponseFormatPicker from './response-format-picker'
 import { RESPONSE_FORMAT } from '../constants/pogues-constants'
 import SimpleResponseFormatEditor from './simple-response-format-editor'
+import SingleResponseFormatEditor from './single-response-format-editor'
 const { SIMPLE, SINGLE, MULTIPLE, TABLE } = RESPONSE_FORMAT
 
 const Editors = {
   SIMPLE: SimpleResponseFormatEditor,
-  SINGLE: () => <div>single</div>,
+  SINGLE: SingleResponseFormatEditor,
   MULTIPLE: () => <div>multiple</div>,
   TABLE: () => <div>table</div>,
 }

src/js/components/simple-response-format-editor.js
@@ -30,10 +30,8 @@ function SimpleResponseFormatEditor(
           locale={locale} />
       </div>
       { DatatypeEditor &&
-        <div className="form-group">
         <DatatypeEditor datatype={format}
           edit={update => changeDatatypeParam(id, update)} locale={locale} />
-      </div>
       }
     </div>
   )

src/js/components/single-response-format-editor.js
@@ -1,0 +1,50 @@
+import React, { PropTypes } from 'react'
+import CodeListSelector from './code-list-selector'
+import VisualizationHintPicker from './visualization-hint-picker'
+
+import {
+  updateSingle, newCodeListSingle
+} from '../actions/response-format'
+
+import { connect } from 'react-redux'
+
+function SingleResponseFormatEditor(
+  { id, qrId, format: { codeListReference, visualizationHint },
+  updateSingle, newCodeListSingle, locale }) {
+  return (
+    <div>
+      <CodeListSelector
+        id={codeListReference}
+        select={codeListReference => updateSingle(id, { codeListReference })}
+        create={() => newCodeListSingle(id, qrId)}
+        locale={locale} />
+      <VisualizationHintPicker visualizationHint={visualizationHint}
+        select={visualizationHint => updateSingle(id, { visualizationHint})}/>
+    </div>
+
+  )
+}
+
+const mapStateToProps = state => ({
+  qrId: state.appState.questionnaire
+})
+
+const mapDispatchToProps = {
+  updateSingle,
+  newCodeListSingle
+}
+
+SingleResponseFormatEditor.propTypes = {
+  id: PropTypes.string.isRequired,
+  /**
+   * Id of the current questionnaire
+   */
+  qrId: PropTypes.string.isRequired,
+  format: PropTypes.object.isRequired,
+  locale: PropTypes.object.isRequired,
+  updateSingle: PropTypes.func.isRequired,
+  newCodeListSingle: PropTypes.func.isRequired
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(SingleResponseFormatEditor)
+

src/js/components/visualization-hint-picker.js
@@ -7,24 +7,25 @@ const vizHintsToText = {
   [DATATYPE_VIZ_HINT.DROPDOWN]: 'dropdown'
 }
 
-export default function VisualizationHintPicker({ visualizationHint, edit }) {
+export default function VisualizationHintPicker({ visualizationHint, select }) {
   return (
-    <div className="col-sm-3">
-      <select onChange={e => edit(e.target.value)}
-            className="form-control"
-            value={visualizationHint}>
-        {
-          _.map(DATATYPE_VIZ_HINT, (hint, value) => {
-            const text = vizHintsToText[value]
-            return <option key={hint} value={value}>{text}</option>
-          })
-        }
-      </select>
+    <div className="form-group">
+      <div className="col-sm-3">
+        <select onChange={e => select(e.target.value)}
+          className="form-control" value={visualizationHint}>
+          {
+            _.map(DATATYPE_VIZ_HINT, (hint, value) => {
+              const text = vizHintsToText[value]
+              return <option key={hint} value={value}>{text}</option>
+            })
+          }
+        </select>
+      </div>
     </div>
   )
 }
 
 VisualizationHintPicker.propTypes = {
   visualizationHint: PropTypes.string.isRequired,
-  edit: PropTypes.func.isRequired
+  select: PropTypes.func.isRequired
 }

src/js/reducers/code-list-by-id.js
@@ -2,6 +2,7 @@ import {
   CREATE_CODE_LIST, REMOVE_CODE_LIST, LOAD_CODE_LIST_SUCCESS, EDIT_CODE_LIST
 } from '../actions/code-list'
 
+import { NEW_CODE_LIST_SINGLE } from '../actions/response-format'
 import { CREATE_CODE, REMOVE_CODE } from '../actions/code'
 import {
   LOAD_CLIST_SPECS_SUCCESS
@@ -24,13 +25,12 @@ const emptyCodeList = {
 export default function (state={}, action) {
   const { type, payload } = action
   switch (type) {
-    case CREATE_CODE_LIST:
+    case NEW_CODE_LIST_SINGLE:
       return {
         ...state,
-        [payload.id]: {
+        [payload.createdClId]: {
           ...emptyCodeList,
-          id: payload.id,
-          label: payload.label
+          id: payload.createdClId
         }
       }
     case CREATE_CODE:
@@ -47,7 +47,7 @@ export default function (state={}, action) {
       //TODO switch to object with aciton handler to avoid conflicts in
       //variables names when we use a `switch` statement
       const codeListIdForRemove = payload.codeListId
-      const codeListForRemove = state[codeListIdForRemove]    
+      const codeListForRemove = state[codeListIdForRemove]
       const codes = [...codeListForRemove.codes]
       const codeId = payload.id
       codes.splice(codes.indexOf(codeId), 1)
@@ -97,7 +97,7 @@ export default function (state={}, action) {
     case REMOVE_CODE_LIST:
       const { [payload.id]: toRemove, ...toKeep } = state
       return toKeep
-    case LOAD_QUESTIONNAIRE_SUCCESS: 
+    case LOAD_QUESTIONNAIRE_SUCCESS:
       return {
         ...state, // to keep code list specifiations
         ...payload.update.codeListById

src/js/reducers/response-format-by-id.js
@@ -1,7 +1,6 @@
 import {
-  SWITCH_FORMAT, CHANGE_DATATYPE_PARAM
+  SWITCH_FORMAT, CHANGE_DATATYPE_PARAM, UPDATE_SINGLE
 } from '../actions/response-format'
-
 import {
   LOAD_QUESTIONNAIRE_SUCCESS
 } from '../actions/questionnaire'
@@ -22,7 +21,7 @@ const emptyFormat = {
   [SIMPLE]: emptyTextDatatype,
   [SINGLE]: {
     codeListReference: '',
-    hint: ''
+    visualizationHint: ''
   },
   [MULTIPLE]: {
 
@@ -71,6 +70,8 @@ const actionsHndlrs = {
   SWITCH_FORMAT: switchFormat,
   CHANGE_DATATYPE_PARAM: fromFormatHndlr(changeDatatypeParam),
   CHANGE_DATATYPE_NAME: fromFormatHndlr(changeDatatypeName),
+  UPDATE_SINGLE: fromFormatHndlr(updateSingle),
+  NEW_CODE_LIST_SINGLE: fromFormatHndlr(newCodeListSingle),
   LOAD_QUESTIONNAIRE_SUCCESS: loadQuestionnaireSuccess
 }
 
@@ -103,25 +104,29 @@ function createComponent(formats, { id, type }) {
   }
 }
 
-
-function editResponse(formats, { id, update }) {
+/**
+ * Update SINGLE format
+ *
+ * Properties to update can include `codeListReference` and `visualizationHint`
+ *
+ * @param  {Object} format             initial format
+ * @param  {Object} payload            action payload
+ * @param  {String} payload.update     properties to update
+ * @return {Object}                    updated format
+ */
+function updateSingle(format, { update }) {
   return {
-    ...formats,
-    [id]: {
-      ...formats[id],
-      ...update
-    }
+    ...format,
+    ...update
   }
 }
 
-function editResponseChooseCodeList(formats, { id, codeListId }) {
-  return editResponse(
-    formats,
-    { id,
-      update: { codeListReference: codeListId }
-    })
+function newCodeListSingle(format, { createdClId }) {
+  return {
+    ...format,
+    codeListReference: createdClId
+  }
 }
-
 function changeDatatypeName(format, { typeName }) {
   if (format.typeName === typeName) return format
   return {

commit b82a7f1d0b6fb81dc4706b1e700cb91f4c8a74ad

Fix error introduced by fd3e232a462fe5323a6dec21c40ae6b399d02595

src/js/reducers/code-list-by-questionnaire.js
@@ -17,7 +17,6 @@ export default function (state={}, action) {
   if (action.type === CREATE_QUESTIONNAIRE) {
     return {
       ...state,
-      [action.payload.qrId]: []
     }
   }
   else if (action.type === NEW_CODE_LIST_SINGLE) {
@@ -28,6 +27,7 @@ export default function (state={}, action) {
     }
   }
   else if (action.type === LOAD_QUESTIONNAIRE_SUCCESS) {
+        [payload.id]: []
     return {
       ...state,
       ...action.payload.update.codeListByQuestionnaire