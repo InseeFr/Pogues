43c6201eb2c455b28cd14c3a5fe47d916576ed3b
Jean-Baptiste RUDANT <jb@noknot.fr>
2016-07-27 15:38:33 +0200

    Upgrade visualization hint picker

--- src/js/components/vis-hint-picker.js
+++ src/js/components/vis-hint-picker.js
@@ -7,15 +7,16 @@ const vizHintsToText = {
   [DATATYPE_VIS_HINT.DROPDOWN]: 'dropdown'
 }
 
-export default function VisHintPicker({ visHint, select,
-  locale }) {
+export default function VisHintPicker(
+  { visHint, select, disabled, locale }) {
   return (
     <div className="form-group">
-      <label htmlFor="visHint" className="col-sm-3 control-label">
+      <label htmlFor="visHint" className="col-sm-5 control-label">
         {locale.visHint}
       </label>
-      <div className="col-sm-3">
+      <div className="col-sm-4">
         <select onChange={e => select(e.target.value)}
+          disabled={disabled}
           className="form-control" value={visHint}
           id="visHint">
           {
@@ -33,5 +34,6 @@ export default function VisHintPicker({ visHint, select,
 VisHintPicker.propTypes = {
   visHint: PropTypes.string.isRequired,
   select: PropTypes.func.isRequired,
-  locale: PropTypes.string.isRequired
+  disabled: PropTypes.bool.isRequired,
+  locale: PropTypes.object.isRequired
 }
