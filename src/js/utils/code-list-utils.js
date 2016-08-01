bfe3a39dce8de6393010f740d90e4d9f6b632da1
Jean-Baptiste RUDANT <jb@noknot.fr>
2016-08-01 20:40:11 +0200

    Improve hack to deal with code list with unknown number of codes

--- src/js/utils/code-list-utils.js
+++ src/js/utils/code-list-utils.js
@@ -1,22 +1,26 @@
+//HACK we avoid return 0 as a length since we will lose information during
+//serialization of a questionnaire (it can lead to generate zero response but
+//responses hold important information such as `codeListReference` or
+//`datatype`).
+
 /**
  * Returns the number of code in a code list from its `id`
  *
  * For a code list specification, it returns `1`.
  * If the `id` is not provided, it returns `1`.
+ * If there is no code in the code list, it returns `1`.
  *
  * Explanations :
  *
  * For a code list specification which has not been loaded yet, we do not know
  * the number of codes in the code list. We need this number in order to
- * serialize the questionnaire (how many `Response` do we need to create ?) and
- * for parsing the questionnaire (if we have a TABLE response format with two
- * information axes and multiple measures, how do we know which response goes
- * with which measure ? => We need this informtion to extract the dataytype from
- * thoses responses). If a code list specification has been loaded (ie
- * `isLoaded` set to `true`), we could know the number of codes, but since we
- * cannot ensure that the specification will be loaded when the questionnaire
- * will be parsed, we cannot rely on this number and use 1 as a convention,
- * whether it has been loaded or not.
+ * serialize the questionnaire (how many `Response` do we need to create ?).
+ *
+ * If a code list specification has been loaded (ie `isLoaded` is `true`), we
+ * could know the number of codes, but since we cannot ensure that the
+ * specification will be loaded when the questionnaire will be parsed and
+ * further updated, we use `1` as a safe convention, whether it has been loaded
+ * or not.
  *
  * If the `id` is not provided, the user might not have selected the code list
  * yet, so we do not know the number of codes. But since this number will be
@@ -24,14 +28,18 @@
  * datatype, we choose to return `1` by default to have at least one response
  * to infer datatype when retrieving a questionnaire.
  *
+ * Same reason to return 1 instead of 0 when there is no code in the code list.
+ *
  * @param  {Object} codeListById collection of code lists (comes from the
  *                               reducer)
  * @param  {String} id           code list id
  * @return {Number}              number of codes in the code list
  */
 export function nbCodesFromId(codeListById, id) {
-  return id  ? nbCodes(codeListById[id]) : 1
+  const nb = id  ? nbCodes(codeListById[id]) : 1
+  return (nb > 0 ? nb : 1)
 }
+
 /**
  * Returns the number of code in a code list
  *
