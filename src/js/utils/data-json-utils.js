a7c7ac48155072e7bc37260346a2886512d2dd5b
Jean-Baptiste RUDANT <jb@noknot.fr>
2016-08-03 16:07:05 +0200

    Fix naming issues when publishing

--- src/js/utils/data-json-utils.js
+++ src/js/utils/data-json-utils.js
@@ -8,13 +8,13 @@ const OBJECT_FIELDS = ['_codeLists'];
 
 // Constants use for normalization
 const CAPS = ['_name', '_survey', '_label', '_datatype', '_codeListReference', '_codeLists', '_codeList', '_codeListSpecification',
-'_maxLength', '_pattern', '_ifTrue', '_ifFalse', '_next', '_expression', '_failMessage','_maximum', '_minimum', '_text'];
+'_maxLength', '_pattern', '_ifTrue', '_ifFalse', '_next', '_expression', '_failMessage','_maximum', '_minimum', '_text', '_responseStructure'];
 
 // Field to rename
 // Mainly fields in plural in Pogues model that need to be singular to be schema valid
 //FIXME only the RENAME_MAPPING is useful, the RENAME const could be replace by Object.keys()
 const RENAME = ['_children', '_responses', '_declarations', '_componentGroups', '_goTos', '_members', '_values', '_codes',
-'_controls', '_visHint'];
+'_controls', '_visHint', '_dimensions'];
 
 const RENAME_MAPPING = {
   '_children':'Child',
@@ -25,7 +25,6 @@ const RENAME_MAPPING = {
   '_goTos' : 'GoTo',
   '_members': 'Member',
   '_responses': 'Response',
-  '_responseStructure': 'ResponseStructure',
   '_dimensions': 'Dimension',
   '_values': 'Value',
   '_visHint': 'VisualizationHint'
