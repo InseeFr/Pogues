8a4b58874710f77846b2bbb4b4f1abe9990ba042
Jean-Baptiste RUDANT <jb@noknot.fr>
2016-09-03 08:12:51 +0200

    Include babel-polyfill

--- src/js/main.js
+++ src/js/main.js
@@ -7,21 +7,8 @@ require('../css/pogues.css')
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from './components/Root'
+import 'babel-polyfill'
 
-if (!String.prototype.includes) {
-  String.prototype.includes = function(search, start) {
-    'use strict';
-    if (typeof start !== 'number') {
-      start = 0;
-    }
-    
-    if (start + search.length > this.length) {
-      return false;
-    } else {
-      return this.indexOf(search, start) !== -1;
-    }
-  };
-}
 
 ReactDOM.render(
 	<Root/>,
