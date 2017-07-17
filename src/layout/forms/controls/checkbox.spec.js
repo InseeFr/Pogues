a65f5fe6c6669542d724d6a2d8c5ec1aed37821a
Emmanuel DEMEY <demey.emmanuel@gmail.com>
2017-07-17 11:16:21 +0200

    Add an utility function when in order to create a decoratedd form for tests

--- src/layout/forms/controls/checkbox.spec.js
+++ src/layout/forms/controls/checkbox.spec.js
@@ -1,27 +1,11 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
-import { combineReducers, createStore } from 'redux';
-import { Provider } from 'react-redux';
-import { reduxForm, reducer as form } from 'redux-form';
 import { Field } from 'redux-form';
 import Checkbox from './checkbox';
-
-function MyForm() {
-  return <Field component={Checkbox} name="checkbox" label="Label Name" />;
-}
+import { decoreFormField } from 'utils/test/test-utils';
 
 describe('Checkbox', () => {
   test('Should have this default template', () => {
-    const store = createStore(combineReducers({ form }), { form: {} });
-
-    const Decorated = reduxForm({ form: 'testForm' })(MyForm);
-    const tree = renderer
-      .create(
-        <Provider store={store}>
-          <Decorated />
-        </Provider>
-      )
-      .toJSON();
+    const tree = decoreFormField(<Field component={Checkbox} name="checkbox" label="Label Name" />).toJSON();
     expect(tree).toMatchSnapshot();
   });
 });
--- src/layout/forms/controls/input.spec.js
+++ src/layout/forms/controls/input.spec.js
@@ -1,43 +1,16 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
-import { combineReducers, createStore } from 'redux';
-import { Provider } from 'react-redux';
-import { reduxForm, reducer as form } from 'redux-form';
+import { decoreFormField } from 'utils/test/test-utils';
 import { Field } from 'redux-form';
 import Input from './input';
 
 describe('Input', () => {
   test('Should have this default template', () => {
-    function MyForm() {
-      return <Field type="text" component={Input} name="input" label="Label Name" />;
-    }
-    const store = createStore(combineReducers({ form }), { form: {} });
-
-    const Decorated = reduxForm({ form: 'testForm' })(MyForm);
-    const tree = renderer
-      .create(
-        <Provider store={store}>
-          <Decorated />
-        </Provider>
-      )
-      .toJSON();
+    const tree = decoreFormField(<Field type="text" component={Input} name="input" label="Label Name" />).toJSON();
     expect(tree).toMatchSnapshot();
   });
 
   test('should have a help block', () => {
-    function MyForm() {
-      return <Field help type="text" component={Input} name="input" label="Label Name" />;
-    }
-    const store = createStore(combineReducers({ form }), { form: {} });
-
-    const Decorated = reduxForm({ form: 'testForm' })(MyForm);
-    const tree = renderer
-      .create(
-        <Provider store={store}>
-          <Decorated />
-        </Provider>
-      )
-      .toJSON();
+    const tree = decoreFormField(<Field help type="text" component={Input} name="input" label="Label Name" />).toJSON();
     expect(tree).toMatchSnapshot();
   });
 });
