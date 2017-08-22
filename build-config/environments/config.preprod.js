5e01aad55c088f56b6458c5c1a6644b2901120fe
Emmanuel DEMEY <demey.emmanuel@gmail.com>
2017-08-22 08:55:39 +0200

    Remove useless config

--- build-config/environments/config.preprod.js
+++ build-config/environments/config.preprod.js
@@ -1,16 +1,3 @@
-function makeRepoURLSpecs() {
-  const repoURL = 'http://dvrmessnclas01.ad.insee.intra:8080';
-  const query = `
-    PREFIX skos:<http://www.w3.org/2004/02/skos/core#>
-    PREFIX xkos:<http://rdf-vocabulary.ddialliance.org/xkos#>
-
-    SELECT ?niveau ?label ?retrievalQuery WHERE {
-       ?niveau a xkos:ClassificationLevel ; skos:prefLabel ?label .
-       BIND(CONCAT("PREFIX skos:<http://www.w3.org/2004/02/skos/core#> PREFIX xkos:<http://rdf-vocabulary.ddialliance.org/xkos#> SELECT ?code ?intitule  WHERE {<", STR(?niveau), "> skos:member ?poste . ?poste skos:notation ?code ; skos:prefLabel ?intitule .} ORDER BY ?code") AS ?retrievalQuery)
-     }`;
-  return `${repoURL}/sparql?query=${encodeURIComponent(query)}`;
-}
-
 const config = {
   dev: true,
   allowRemovalOfQuestionnaire: false,
@@ -21,9 +8,6 @@ const config = {
   log: {
     level: 'DEBUG',
     activeNamespaces: ['Components', 'Actions', 'Stores', 'Utils', 'Models'],
-  },
-  codeLists: {
-    repoURLSpecs: makeRepoURLSpecs(),
   },
 };
 
