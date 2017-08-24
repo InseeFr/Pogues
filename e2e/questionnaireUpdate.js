7b3c84d6bc56ad5b38e85b17c5908e725d5e8885
Emmanuel DEMEY <demey.emmanuel@gmail.com>
2017-08-24 15:12:27 +0200

    Add Page Object for Question page

--- e2e/questionnaireUpdate.js
+++ e2e/questionnaireUpdate.js
@@ -1,17 +1,18 @@
 const config = require('../nightwatch.conf.js');
+const questionnairePage = require('./po/questionnaire');
 
 module.exports = {
   'Application assert Title': browser => {
     browser
       .url("http://localhost:3000/#/questionnaire/fr.insee-POPO-QPO-DOC")
       .waitForElementVisible('body')
-      .click('#questionnaire-head button:nth-child(1)')
-      .waitForElementVisible('#input-label')
-      .clearValue('.popup-body #input-label')
-      .setValue('.popup-body #input-label', 'nightwatch')
-      .click('.popup-body button[type=submit]')
-      .waitForElementNotPresent('#input-label')
-      .assert.containsText('h4', 'nightwatch')
+      .click(questionnairePage.VoirLeDetailButton)
+      .waitForElementVisible(questionnairePage.QuestionnaireModalLabelInput)
+      .clearValue(questionnairePage.QuestionnaireModalLabelInput)
+      .setValue(questionnairePage.QuestionnaireModalLabelInput, 'nightwatch')
+      .click(questionnairePage.QuestionnaireModalSubmit)
+      .waitForElementNotPresent(questionnairePage.QuestionnaireModalLabelInput)
+      .assert.containsText(questionnairePage.QuestionnaireTitle, 'nightwatch')
       .end()
   },
 };
