a8afd482bec61c69159ef1ef3c78e1cc17c7ee02
Jose Javier Linan Manzaneda <javier.linmanzaneda@gmail.com>
2017-09-25 11:27:45 +0200

    [FIX] Fixing calculated and collected variables saving process.

--- src/utils/variables/variables-utils.js
+++ src/utils/variables/variables-utils.js
@@ -2,7 +2,7 @@ import { COMPONENT_TYPE } from 'constants/pogues-constants';
 
 const { QUESTION } = COMPONENT_TYPE;
 
-export function removeOrphansVariables(variablesIdsFromComponents, variablesStore = {}) {
+export function removeOrphansCollectedVariables(variablesIdsFromComponents, variablesStore = {}) {
   return Object.keys(variablesStore)
     .filter(key => variablesIdsFromComponents.indexOf(key) !== -1)
     .reduce((acc, key) => {
@@ -13,13 +13,12 @@ export function removeOrphansVariables(variablesIdsFromComponents, variablesStor
     }, {});
 }
 
-export function getVariablesIdsFromComponents(componentsStore) {
+export function getCollectedVariablesIdsFromComponents(componentsStore) {
   return Object.keys(componentsStore)
     .filter(key => {
       return componentsStore[key].type === QUESTION;
     })
     .reduce((acc, key) => {
-      const { collectedVariables, calculateVariables, externalVariables } = componentsStore[key];
-      return [...acc, ...(collectedVariables || []), ...(calculateVariables || []), ...(externalVariables || [])];
+      return [...acc, ...(componentsStore[key].collectedVariables || [])];
     }, []);
 }
