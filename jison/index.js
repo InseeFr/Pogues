17ea77c3f3634ef00a6108071a91f88e9c891cdb
qlevaslot <quentin.levaslot@gmail.com>
2017-10-19 16:04:25 +0200

    [NEW] Add HoC for XPath validation

--- jison/index.js
+++ jison/index.js
@@ -1,26 +1,32 @@
 /**
- * Define new grammars in /grammars if needed.
+ * Define new grammars in grammars/ if needed.
+ *
  * Generate grammar parsers through `npm run jison`
- * Committing built generated parsers for now, since they are supposed to be quite static
+ * After the generation, you MUST delete `_token_stack:` line (pending PR #352 of jison project)
+ * and the export.main function definition. Sorry.
+ *
+ * Committing built generated parsers for now,
+ * since they are supposed to be quite static and avoids aforementioned work
  * Wrap generated parsers and export functions here to be used in Pogues.
  *
  * Based on dimagi/js-xpath project on github, in a nicer package and with less functionalities to fit our needs.
  */
-import xpathParser from 'jison/generated-parsers/xpath-parser';
+import xpathParser from './generated-parsers/xpath-parser';
 
 /**
  * Return null if given a valid expression, text otherwise
- * @param xpath
- * @returns {*}
+ * @param xpath expression
+ * @returns error message if anything went wrong during the parsing of the expression
  */
 const validateXpath = (xpath) => {
-  try {
-    // Throws an error if the syntax isn't valid
-    xpathParser.parse(xpath);
-  } catch (e) {
-    return e.message;
+  if(xpath) {
+    try {
+      // Throws an error if the syntax isn't valid
+      xpathParser.parse(xpath);
+    } catch (e) {
+      return e.message;
+    }
   }
-  return null;
 };
 
 export { validateXpath };
