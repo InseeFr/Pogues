5a66cd0ea4bd87981618e1bcde88f0929cd48c45
Jose Javier Linan Manzaneda <javier.linmanzaneda@gmail.com>
2017-11-06 12:20:33 +0100

    [FIX] Fixing tests

--- src/widgets/codes-lists/index.js
+++ src/widgets/codes-lists/index.js
@@ -1,3 +1,2 @@
 export { default as CodesLists } from './containers/codes-lists-container';
-export { default as CodesListFactory } from './model/codes-list';
 export { defaultCodesList } from './model/codes-list';
