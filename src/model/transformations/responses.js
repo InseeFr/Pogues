1fa58469ba950191f4100120e67e3ca7dd0986e6
NicoLaval <nicolas.laval@insee.fr>
2018-01-15 14:30:23 +0100

    Update declaration (Trello #199)

--- src/model/transformations/responses.js
+++ src/model/transformations/responses.js
@@ -6,7 +6,8 @@ export function stateToModel(state, collectedVariables, collectedVariablesStore,
   const mappingModel = responsesModel.map(r => {
     const x = collectedVariablesStore[r.CollectedVariableReference].x;
     const y = collectedVariablesStore[r.CollectedVariableReference].y;
-    const MappingTarget = type === QUESTION_TYPE_ENUM.MULTIPLE_CHOICE ? `${x}` : `${x} ${y}`;
+    // Table : Fix column and look into lines
+    const MappingTarget = type === QUESTION_TYPE_ENUM.MULTIPLE_CHOICE ? `${y}` : `${y} ${x}`;
     return ({ MappingSource: r.id, MappingTarget, });
   });
 
