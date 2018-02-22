dab8da7677e7d6c5e6f891b31d1b233ecba4e5c4
Emmanuel Demey <demey.emmanuel@gmail.com>
2018-02-22 08:17:21 +0100

    Add unit test for errors integrity reducer

---
+++ src/reducers/errors/errors-integrity.spec.js
@@ -0,0 +1,42 @@
+import actionsHandlers, { removeError } from './errors-integrity';
+import { REMOVE_INTEGRITY_ERROR } from 'actions/errors';
+
+describe('removeError', () => {
+  test('when called directly', () => {
+    const result = removeError(
+      {
+        '1': {
+          typeError: [{ itemListId: '2' }],
+          typeError2: [{ itemListId: '3' }]
+        },
+        '2': { typeError2: [{ itemListId: '1' }] }
+      },
+      { componentId: '1', typeError: 'typeError', itemListId: '2' }
+    );
+    expect(result).toEqual({
+      '1': { typeError: [], typeError2: [{ itemListId: '3' }] },
+      '2': { typeError2: [{ itemListId: '1' }] }
+    });
+  });
+  [REMOVE_INTEGRITY_ERROR].forEach(action => {
+    test(`when called when we trigger ${action}`, () => {
+      const result = actionsHandlers(
+        {
+          '1': {
+            typeError: [{ itemListId: '2' }],
+            typeError2: [{ itemListId: '3' }]
+          },
+          '2': { typeError2: [{ itemListId: '1' }] }
+        },
+        {
+          type: action,
+          payload: { componentId: '1', typeError: 'typeError', itemListId: '2' }
+        }
+      );
+      expect(result).toEqual({
+        '1': { typeError: [], typeError2: [{ itemListId: '3' }] },
+        '2': { typeError2: [{ itemListId: '1' }] }
+      });
+    });
+  });
+});
