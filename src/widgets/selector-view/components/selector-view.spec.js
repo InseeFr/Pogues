1ab13538eab5c5971a962f6f2c5057b410280c94
Emmanuel Demey <demey.emmanuel@gmail.com>
2018-02-25 17:15:03 +0100

    feat: add unit test for the selector-view component

---
+++ src/widgets/selector-view/components/selector-view.spec.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import SelectorView from './selector-view';
+import { decoreFormField } from 'utils/test/test-utils.jsx';
+
+describe('Tab', () => {
+  test('Should exists with the corresponding template with select', () => {
+    const props = {
+      activeViewValue: 'value2',
+      label: 'fake label',
+      radio: false
+    };
+    const selectorView = renderer
+      .create(
+        decoreFormField(
+          <SelectorView {...props}>
+            <div value={'value1'} label={'label1'}>
+              Fake Children
+            </div>
+            <div value={'value2'} label={'label2'}>
+              Fake Children2
+            </div>
+          </SelectorView>
+        )
+      )
+      .toJSON();
+
+    expect(selectorView).toMatchSnapshot();
+  });
+  test('Should exists with the corresponding template with radio', () => {
+    const props = {
+      activeViewValue: 'value2',
+      label: 'fake label',
+      radio: true
+    };
+    const selectorView = renderer
+      .create(
+        decoreFormField(
+          <SelectorView {...props}>
+            <div value={'value1'} label={'label1'}>
+              Fake Children
+            </div>
+            <div value={'value2'} label={'label2'}>
+              Fake Children2
+            </div>
+          </SelectorView>
+        )
+      )
+      .toJSON();
+
+    expect(selectorView).toMatchSnapshot();
+  });
+});
