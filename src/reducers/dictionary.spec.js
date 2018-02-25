c1282e103c57781590072db4f6a72c82bed44d68
Emmanuel Demey <demey.emmanuel@gmail.com>
2018-02-25 17:11:18 +0100

    Add unit test for dictionary reducer

---
+++ src/reducers/dictionary.spec.js
@@ -0,0 +1,14 @@
+import Dictionary, { getLocale } from './dictionary';
+
+describe('Dictionary', () => {
+  test('should return the frech message', () => {
+    expect(Dictionary().questionnaireValid).toEqual(
+      'Your questionnaire is valid'
+    );
+  });
+});
+describe('getLocale', () => {
+  test('should return the frech message', () => {
+    expect(getLocale()).toEqual('en');
+  });
+});
