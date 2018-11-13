bd4fddfd71dd4890b39c9d0e96e9c93c850ca7d9
Emmanuel Demey <demey.emmanuel@gmail.com>
2018-11-13 14:53:16 +0100

    feat: add links to the help page

--- src/layout/page-help/index.js
+++
@@ -1,1 +0,0 @@
-export { default as PageHelp } from './page-help';
--- src/router.jsx
+++ src/router.jsx
@@ -3,7 +3,6 @@ import { Router, hashHistory, Route, IndexRoute, Redirect } from 'react-router';
 
 import { App } from 'layout/app';
 import { PageHome } from 'layout/page-home';
-import { PageHelp } from 'layout/page-help';
 import { PageSearchQuestionnaire } from 'layout/page-search-questionnaire';
 import { PageQuestionnaire } from 'layout/page-questionnaire';
 
@@ -12,7 +11,6 @@ function RouterContainer() {
     <Router history={hashHistory}>
       <Route path="/" component={App}>
         <IndexRoute component={PageHome} />
-        <Route exact path="/help" component={PageHelp} />
         <Route exact path="/questionnaire/:id" component={PageQuestionnaire} />
         <Route
           exact
