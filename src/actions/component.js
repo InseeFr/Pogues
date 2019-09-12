80670489d5689d559a0cc73fa7bf76ec607d4bf6
Emmanuel Demey <demey.emmanuel@gmail.com>
2019-09-12 10:55:54 +0200

    fix: migrate to babel 7

--- src/actions/component.js
+++ src/actions/component.js
@@ -113,7 +113,7 @@ export const orderComponents = ({ payload: { id, lastCreatedComponent } }) => (
   const {
     selectedComponentId,
     activeComponentsById: activesComponents,
-  } = state.appState.selectedComponentId;
+  } = state.appState;
   const selectedComponent = activesComponents[selectedComponentId];
 
   let activeComponentsById = {};
