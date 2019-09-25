842e9488ea1788a2d56be682a40cf213d81cb23d
benoit <benoit.werquin@insee.fr>
2019-09-25 15:42:48 +0200

    remove unused code

--- build-config/environments/config.dev.js
+++ build-config/environments/config.dev.js
@@ -9,15 +9,3 @@ module.exports = {
     activeNamespaces: ['Components', 'Actions', 'Stores', 'Utils', 'Models'],
   },
 };
-
-// module.exports = {
-//   dev: true,
-//   allowRemovalOfQuestionnaire: true,
-//   baseURL: 'http://dvrmspogfolht01.ad.insee.intra/rmspogfo/pogues',
-//   persistancePath: '/persistence',
-//   userPath: '/user',
-//   log: {
-//     level: 'DEBUG',
-//     activeNamespaces: ['Components', 'Actions', 'Stores', 'Utils', 'Models']
-//   },
-// };
