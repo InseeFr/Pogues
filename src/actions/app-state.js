2b9203799da2bb6f6fca56c35cd15a57fffdb5ab
Loïc Henninger <loic.henninger@insee.fr>
2021-05-07 16:28:15 +0200

    Stamp of the owner of a questionnaire stays the same and do not take the stamp of the current user

--- src/actions/app-state.js
+++ src/actions/app-state.js
@@ -8,7 +8,6 @@ import {
 } from 'utils/remote-api';
 import { questionnaireRemoteToStores } from 'model/remote-to-stores';
 import * as Questionnaire from 'model/transformations/questionnaire';
-import { getUser } from 'reducers/selectors';
 
 export const SET_ACTIVE_QUESTIONNAIRE = 'SET_ACTIVE_QUESTIONNAIRE';
 export const SET_ACTIVE_COMPONENTS = 'SET_ACTIVE_COMPONENTS';
@@ -266,7 +265,6 @@ function getQuestionnaireModel(state, customComponentsStore) {
   const questionnaireState = {
     ...state.appState.activeQuestionnaire,
     lastUpdatedDate: new Date().toString(),
-    owner: getUser(state).stamp,
   };
 
   return Questionnaire.stateToRemote(questionnaireState, stores);
@@ -389,7 +387,6 @@ export const visualizeActiveQuestionnaire = (type, componentId, token) => {
           ),
         )
       : state.appState.activeComponentsById;
-
     const questionnaireModel = getQuestionnaireModel(state, componentsById);
     if (type === 'pdf') {
       visualizePdf(questionnaireModel, token);
