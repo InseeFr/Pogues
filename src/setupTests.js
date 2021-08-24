abdb802a488b54e549e41bd4770bc524908f3c4b
NicoLaval <nicolas.laval@insee.fr>
2021-08-24 18:21:01 +0200

    Remove unused

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,9 +1,4 @@
 import Adapter from 'enzyme-adapter-react-16';
 import { configure } from 'enzyme';
-import { JSDOM } from 'jsdom';
-
-const dom = new JSDOM();
-global.document = dom.window.document;
-global.window = dom.window;
 
 configure({ adapter: new Adapter() });
