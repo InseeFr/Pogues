2d209c3ce4cc3f399d26e755047b0dfdaa13bb49
Loïc Henninger <loic.henninger@insee.fr>
2022-01-10 15:39:09 +0100

    Update questionnaire.js

--- src/actions/questionnaire.js
+++ src/actions/questionnaire.js
@@ -310,32 +310,36 @@ export const mergeQuestions = (idMerge, token) => (dispatch, getState) => {
       activeExternalVariablesById[variable.id] = variable;
     });
     const supSequence = getSupWeight(activeComponentsById);
+    debugger;
     Object.values(mergesComponentByQuestionnaire)
       .filter(
         element =>
           element.type !== QUESTIONNAIRE && element.id !== 'idendquest',
       )
       .forEach(component => {
-        const find = Object.values(activeComponentsById).find(
+        const findName = Object.values(activeComponentsById).find(
           active => active.name === component.name,
         );
-        if (find) {
-          if (find.id === component.id) {
-            component.id = uuid();
-            Object.values(mergesComponentByQuestionnaire).forEach(element => {
-              if (element.parent === find.id) {
-                element.parent = component.id;
-              }
-              if (
-                element.children?.length > 0 &&
-                element.children.includes(find.id)
-              ) {
-                const index = element.children.indexOf(find.id);
-                element.children[index] = component.id;
-              }
-            });
-          }
+        if (findName) {
           component.name = `${component.name}_2`;
+        }
+        const findId = Object.values(activeComponentsById).find(
+          active => active.id === component.id,
+        );
+        if (findId) {
+          component.id = uuid();
+          Object.values(mergesComponentByQuestionnaire).forEach(element => {
+            if (element.parent === findId.id) {
+              element.parent = component.id;
+            }
+            if (
+              element.children?.length > 0 &&
+              element.children.includes(findId.id)
+            ) {
+              const index = element.children.indexOf(findId.id);
+              element.children[index] = component.id;
+            }
+          });
         }
         const collectedVaribles = {};
         if (component.type === SEQUENCE) {
