e1eb95be8fe500c8435d007fdbd0cc4f781e4052
Bulot François <f_bulot@hotmail.com>
2023-02-08 15:54:05 +0100

    update isLoopsValid

--- src/layout/generic-input/containers/generic-input-container.js
+++ src/layout/generic-input/containers/generic-input-container.js
@@ -51,11 +51,22 @@ function isQuestionnaireValid(questionnaireErrors = {}) {
   );
 }
 
-function isLoopsValid(componentsStore) {
+function isLoopsValid(
+  componentsStore,
+  activeQuestionnaire,
+  externalQuestionnairesLoops,
+) {
   let loopsValid = true;
   const componentsLoop = Object.values(componentsStore).filter(
     component => component.type === LOOP,
   );
+  const externalLoopsAvailable = externalQuestionnairesLoops || {};
+  const externalQuestionnnairesId =
+    activeQuestionnaire.childQuestionnaireRef || [];
+  const referencedLoops = Object.keys(externalLoopsAvailable)
+    .filter(key => externalQuestionnnairesId.includes(key))
+    .reduce((acc, key) => [...acc, ...externalLoopsAvailable[key].loops], []);
+
   if (componentsLoop.length > 0) {
     componentsLoop.forEach(component => {
       if (
@@ -63,7 +74,9 @@ function isLoopsValid(componentsStore) {
         !componentsStore[component.finalMember] ||
         componentsStore[component.initialMember].weight >
           componentsStore[component.finalMember].weight ||
-        (component.basedOn && !componentsStore[component.basedOn])
+        (component.basedOn &&
+          !componentsStore[component.basedOn] &&
+          !referencedLoops.some(loop => loop.id === component.basedOn))
       ) {
         loopsValid = false;
       }
@@ -77,6 +90,7 @@ function isLoopsValid(componentsStore) {
 const mapStateToProps = state => {
   const { activeComponentsById, selectedComponentId, activeQuestionnaire } =
     state.appState;
+  const { externalQuestionnairesLoops } = state.metadataByType;
   const errors = state.errors || { errorsIntegrity: {} };
   const questionnaireErrors =
     errors.errorsIntegrity[activeQuestionnaire.id] || {};
@@ -91,7 +105,11 @@ const mapStateToProps = state => {
     isQuestionnaireHaveError: state.appState.isQuestionnaireHaveError,
     isQuestionnaireModified: state.appState.isQuestionnaireModified,
     isQuestionnaireValid: isQuestionnaireValid(questionnaireErrors),
-    isLoopsValid: isLoopsValid(activeComponentsById),
+    isLoopsValid: isLoopsValid(
+      activeComponentsById,
+      activeQuestionnaire,
+      externalQuestionnairesLoops,
+    ),
     activeQuestionnaire: activeQuestionnaire,
     token: getToken(state),
     showVisualizationErrorPopup:
