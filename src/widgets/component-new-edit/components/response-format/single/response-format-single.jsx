ce68c2d8f6d7a5f4ee31b71eae25f7dc6f9c53f2
Bulot François <f_bulot@hotmail.com>
2023-10-11 10:15:04 +0200

    remove code smells

--- src/widgets/component-new-edit/components/response-format/single/response-format-single.jsx
+++ src/widgets/component-new-edit/components/response-format/single/response-format-single.jsx
@@ -174,6 +174,8 @@ ResponseFormatSingle.propTypes = {
   showMandatory: PropTypes.bool,
   componentsStore: PropTypes.object,
   collectedVariablesStore: PropTypes.object,
+  visHint: PropTypes.string,
+  path: PropTypes.string,
   formName: PropTypes.string,
 };
 
@@ -183,6 +185,8 @@ ResponseFormatSingle.defaultProps = {
   showMandatory: true,
   componentsStore: {},
   collectedVariablesStore: {},
+  visHint: undefined,
+  path: SINGLE_CHOICE,
   formName: DEFAULT_FORM_NAME,
 };
 
