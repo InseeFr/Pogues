ee452d0554e4481234b5c10509c42a44b6fd86c5
Laurent Caouissin <caouissin.laurent@gmail.com>
2023-12-13 10:28:37 +0100

    Switch to oidc-sap library

---
+++ src/utils/oidc/useAuth.js
@@ -0,0 +1,44 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+import { AuthContext } from 'components/auth/provider/component';
+import { decodeJwt } from 'oidc-spa';
+import { useOidc } from 'oidc-spa/react';
+import { useContext, useMemo } from 'react';
+import { getEnvVar } from 'utils/env';
+
+export const useAuth = authType => {
+  if (authType === 'OIDC') {
+    return useOidc();
+  } else {
+    const dummyClient = useContext(AuthContext);
+    return { oidc: dummyClient };
+  }
+};
+
+export const useUser = authType => {
+  const { oidc } = useAuth();
+
+  if (!oidc.isUserLoggedIn) {
+    throw new Error('This hook should be used only on authenticated routes');
+  }
+
+  const { idToken } = oidc.getTokens();
+
+  const user = useMemo(() => {
+    if (authType === 'OIDC') {
+      const {
+        preferred_username: id,
+        name,
+        timbre: stamp,
+      } = decodeJwt(idToken);
+      return { id, name, stamp };
+    }
+    return {
+      id: getEnvVar('DEFAULT_USER_ID') || 'FAKEID',
+      name: getEnvVar('DEFAULT_USER_NAME') || 'Fake user',
+      stamp: getEnvVar('DEFAULT_USER_STAMP') || 'FAKEPERMISSION',
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [idToken]);
+
+  return { user };
+};
