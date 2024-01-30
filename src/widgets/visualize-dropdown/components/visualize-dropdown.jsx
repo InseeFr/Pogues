93b4b9eb942732bc1cc26154cf72af478adfcf6d
Laurent Caouissin <38245508+laurentC35@users.noreply.github.com>
2024-01-30 17:14:34 +0100

    Feat/stromae v3 (#727)
    * feat: Add stromaeV3 visu
    
    This reverts commit f3a399a79e129560e4e67c4d91a91bf4bc7c5eac.
    
    * bumb version to 1.5.7
    
    * Update package.json

--- src/widgets/visualize-dropdown/components/visualize-dropdown.jsx
+++ src/widgets/visualize-dropdown/components/visualize-dropdown.jsx
@@ -83,11 +83,11 @@ function VisualizeDropdown({
       actionType: 'stromae-v2',
       actionLabel: Dictionary.VISUALIZE_WEB_STROMAE_V2,
     },
-    /* {
+    {
       actionType: 'stromae-v3',
       actionLabel: Dictionary.VISUALIZE_WEB_STROMAE_V3,
       tag: 'beta',
-    }, */
+    },
     { actionType: 'queen-capi', actionLabel: Dictionary.VISUALIZE_QUEEN_CAPI },
     { actionType: 'queen-cati', actionLabel: Dictionary.VISUALIZE_QUEEN_CATI },
     { actionType: 'pdf', actionLabel: Dictionary.VISUALIZE_PDF },
