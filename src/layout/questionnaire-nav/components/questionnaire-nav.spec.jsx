50dc4a7f330633f8320574d4062f088bc3b0ecf5
Quentin Ruhier <quentin.ruhier@insee.fr>
2024-06-27 16:48:47 +0200

    test: fix QuestionnaireNav tests

--- src/layout/questionnaire-nav/components/questionnaire-nav.spec.jsx
+++ src/layout/questionnaire-nav/components/questionnaire-nav.spec.jsx
@@ -1,9 +1,8 @@
-import { shallow } from 'enzyme';
+import { render } from '@testing-library/react';
 import React from 'react';
 import { describe, expect, test, vi } from 'vitest';
-
+import { BrowserRouter as Router } from 'react-router-dom';
 import QuestionnaireNav from './questionnaire-nav';
-
 import { QUESTIONNAIRE_NAV } from '../../../constants/dom-constants';
 import { noop } from '../../../utils/test/test-utils';
 
@@ -11,7 +10,7 @@ const { COMPONENT_CLASS } = QUESTIONNAIRE_NAV;
 
 // We need to mock this import, otherwise the import of VTL-Editor crashes the test
 
-vi.mock('../component-edit', () => {
+vi.mock('../../component-edit', () => {
   return {
     __esModule: true,
     default: () => {
@@ -24,16 +23,21 @@ vi.mock('../component-edit', () => {
 });
 
 describe('<QuestionnaireNav />', () => {
-  const props = {
-    questionnaire: {},
-    componentsStore: {},
-    setSelectedComponentId: noop,
-    removeComponent: () => {},
-  };
-
-  const wrapper = shallow(<QuestionnaireNav {...props} />);
-
   test('should render without throwing an error', () => {
-    expect(wrapper.is(`.${COMPONENT_CLASS}`)).toBe(true);
+    const props = {
+      questionnaire: {},
+      componentsStore: {},
+      setSelectedComponentId: noop,
+      removeComponent: () => {},
+    };
+
+    render(
+      <Router>
+        <QuestionnaireNav {...props} />
+      </Router>,
+    );
+
+    const questionnaireNav = document.querySelector(`.${COMPONENT_CLASS}`);
+    expect(questionnaireNav).not.toBeNull();
   });
 });
