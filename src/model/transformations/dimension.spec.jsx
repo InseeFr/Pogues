3243fc7748142480d2e1379ac5d07476d5df3903
Quentin Ruhier <quentin.ruhier@insee.fr>
2024-09-23 14:11:31 +0200

    test: fix tests

--- src/model/transformations/dimension.spec.jsx
+++ src/model/transformations/dimension.spec.jsx
@@ -30,88 +30,129 @@ describe('dimension tranformations', () => {
     });
   });
 
-  [PRIMARY, SECONDARY].forEach(type => {
-    test(`when the type is ${type} and has a CodesListState`, () => {
-      const result = stateToRemote({
-        type: type,
-        [DEFAULT_CODES_LIST_SELECTOR_PATH]: { id: '1' },
-      });
-
-      expect(result).toEqual({
-        dynamic: '0',
-        dimensionType: type,
-        CodeListReference: '1',
-      });
+  test(`when the type is PRIMARY and has a CodesListState`, () => {
+    const result = stateToRemote({
+      type: PRIMARY,
+      [DEFAULT_CODES_LIST_SELECTOR_PATH]: { id: '1' },
     });
-    test(`when the type is ${type} and has a totalLabel and showTotalLabel=true`, () => {
-      const result = stateToRemote({
-        type: type,
-        totalLabel: 'totalLabel',
-        showTotalLabel: true,
-      });
 
-      expect(result).toEqual({
-        dynamic: '0',
-        dimensionType: type,
-        totalLabel: 'totalLabel',
-      });
+    expect(result).toEqual({
+      dynamic: 'NON_DYNAMIC',
+      dimensionType: PRIMARY,
+      CodeListReference: '1',
     });
-    test(`when the type is ${type} and has a totalLabel and showTotalLabel=false`, () => {
-      const result = stateToRemote({
-        type: type,
-        totalLabel: 'totalLabel',
-        showTotalLabel: false,
-      });
-
-      expect(result).toEqual({
-        dynamic: '0',
-        dimensionType: type,
-      });
+  });
+  test(`when the type is PRIMARY and has a totalLabel and showTotalLabel=true`, () => {
+    const result = stateToRemote({
+      type: PRIMARY,
+      totalLabel: 'totalLabel',
+      showTotalLabel: true,
     });
-    test(`when the type is ${type} and has a numLinesMin and numLinesMax`, () => {
-      const result = stateToRemote({
-        type: type,
-        numLinesMin: 1,
-        numLinesMax: 2,
-      });
 
-      expect(result).toEqual({
-        dimensionType: type,
-        dynamic: '1-2',
-      });
+    expect(result).toEqual({
+      dynamic: 'NON_DYNAMIC',
+      dimensionType: PRIMARY,
+      totalLabel: 'totalLabel',
     });
-    test(`when the type is ${type} and has a numLinesMin but not numLinesMax`, () => {
-      const result = stateToRemote({
-        type: type,
-        numLinesMin: 1,
-      });
-
-      expect(result).toEqual({
-        dynamic: '0',
-        dimensionType: type,
-      });
+  });
+  test(`when the type is PRIMARY and has a totalLabel and showTotalLabel=false`, () => {
+    const result = stateToRemote({
+      type: PRIMARY,
+      totalLabel: 'totalLabel',
+      showTotalLabel: false,
     });
-    test(`when the type is ${type} and has a numLinesMax but not numLinesMin`, () => {
-      const result = stateToRemote({
-        type: type,
-        numLinesMax: 2,
-      });
 
-      expect(result).toEqual({
-        dynamic: '0',
-        dimensionType: type,
-      });
+    expect(result).toEqual({
+      dynamic: 'NON_DYNAMIC',
+      dimensionType: PRIMARY,
+    });
+  });
+  test(`when the type is PRIMARY and has a numLinesMin and numLinesMax`, () => {
+    const result = stateToRemote({
+      type: PRIMARY,
+      isFixedLength: '0',
+      fixedLength: 'formula',
+      numLinesMin: 1,
+      numLinesMax: 2,
+    });
+
+    expect(result).toEqual({
+      dimensionType: PRIMARY,
+      dynamic: 'DYNAMIC_LENGTH',
+      isFixedLength: false,
+      minimum: 1,
+      maximum: 2,
+    });
+  });
+  test(`when the type is PRIMARY and has a numLinesMin but not numLinesMax`, () => {
+    const result = stateToRemote({
+      type: PRIMARY,
+      numLinesMin: 1,
+    });
+
+    expect(result).toEqual({
+      dynamic: 'NON_DYNAMIC',
+      dimensionType: PRIMARY,
+    });
+  });
+  test(`when the type is PRIMARY and has a numLinesMax but not numLinesMin`, () => {
+    const result = stateToRemote({
+      type: PRIMARY,
+      numLinesMax: 2,
+    });
+
+    expect(result).toEqual({
+      dynamic: 'NON_DYNAMIC',
+      dimensionType: PRIMARY,
     });
   });
 
-  test('when the type is not PRIMARY, SECONDATY neither MEASURE', () => {
+  test(`when the type is SECONDARY and has a CodesListState`, () => {
     const result = stateToRemote({
-      type: 'FAKE TYPE',
+      type: SECONDARY,
+      [DEFAULT_CODES_LIST_SELECTOR_PATH]: { id: '1' },
     });
 
     expect(result).toEqual({
       dynamic: '0',
-      dimensionType: 'FAKE TYPE',
+      dimensionType: SECONDARY,
+      CodeListReference: '1',
     });
+  });
+  test(`when the type is SECONDARY and has a totalLabel and showTotalLabel=true`, () => {
+    const result = stateToRemote({
+      type: SECONDARY,
+      totalLabel: 'totalLabel',
+      showTotalLabel: true,
+    });
+
+    expect(result).toEqual({
+      dynamic: '0',
+      dimensionType: SECONDARY,
+      totalLabel: 'totalLabel',
+    });
+  });
+  test(`when the type is PRIMARY and has a totalLabel and showTotalLabel=false`, () => {
+    const result = stateToRemote({
+      type: SECONDARY,
+      totalLabel: 'totalLabel',
+      showTotalLabel: false,
+    });
+
+    expect(result).toEqual({
+      dynamic: '0',
+      dimensionType: SECONDARY,
+    });
+  });
+});
+
+test('when the type is not PRIMARY, SECONDATY neither MEASURE', () => {
+  const result = stateToRemote({
+    type: 'FAKE TYPE',
+  });
+
+  expect(result).toEqual({
+    dynamic: '0',
+    dimensionType: 'FAKE TYPE',
   });
 });
