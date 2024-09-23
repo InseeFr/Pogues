d6910276779adfd4b3955206c6aff6afd06f57f8
Quentin Ruhier <quentin.ruhier@insee.fr>
2024-09-23 11:04:26 +0200

    feat: add VTL formula for primary table dimension

--- src/widgets/component-new-edit/components/response-format/table/table-primary-list.jsx
+++ src/widgets/component-new-edit/components/response-format/table/table-primary-list.jsx
@@ -1,33 +1,77 @@
 import React from 'react';
-import { FormSection, Field } from 'redux-form';
+import { FormSection, Field, formValueSelector } from 'redux-form';
 
 import Dictionary from '../../../../../utils/dictionary/dictionary';
 import Input from '../../../../../forms/controls/input';
 import { DIMENSION_FORMATS } from '../../../../../constants/pogues-constants';
+import { InputWithVariableAutoCompletion } from '../../../../../forms/controls/control-with-suggestions';
+import ListRadios from '../../../../../forms/controls/list-radios';
+import GenericOption from '../../../../../forms/controls/generic-option';
+import { connect } from 'react-redux';
+import withCurrentFormVariables from '../../../../../hoc/with-current-form-variables';
 
 const { LIST: selectorPath } = DIMENSION_FORMATS;
 
-function ResponseFormatTablePrincipalList() {
+function ResponseFormatTablePrincipalList({ isFixedLength }) {
   return (
     <div className="axis-primary__panel">
       <FormSection name={selectorPath}>
         <Field
-          name="numLinesMin"
-          type="number"
-          component={Input}
-          label={Dictionary.minRowNb}
+          name="isFixedLength"
+          label={Dictionary.linesNbCalculation}
+          component={ListRadios}
           required
-        />
-        <Field
-          name="numLinesMax"
-          type="number"
-          component={Input}
-          label={Dictionary.maxRowNb}
-          required
-        />
+        >
+          <GenericOption key="0" value="0">
+            {Dictionary.minMax}
+          </GenericOption>
+          <GenericOption key="1" value="1">
+            {Dictionary.formula}
+          </GenericOption>
+        </Field>
+        {isFixedLength === '1' ? (
+          <Field
+            name="fixedLength"
+            type="text"
+            component={InputWithVariableAutoCompletion}
+            label={Dictionary.formula}
+            required
+          />
+        ) : (
+          <div>
+            <Field
+              name="numLinesMin"
+              type="number"
+              component={Input}
+              label={Dictionary.minRowNb}
+              required
+            />
+            <Field
+              name="numLinesMax"
+              type="number"
+              component={Input}
+              label={Dictionary.maxRowNb}
+              required
+            />
+          </div>
+        )}
       </FormSection>
     </div>
   );
 }
 
-export default ResponseFormatTablePrincipalList;
+// Container
+const mapStateToProps = (state, { selectorPathParent }) => {
+  const selector = formValueSelector('component');
+
+  return {
+    isFixedLength: selector(
+      state,
+      `${selectorPathParent}.${selectorPath}.isFixedLength`,
+    ),
+  };
+};
+
+export default connect(mapStateToProps)(
+  withCurrentFormVariables(ResponseFormatTablePrincipalList),
+);
