94232e51dd40232608da3c7fce1e3c33f09c48f8
Quentin Ruhier <quentin.ruhier@insee.fr>
2024-10-22 15:37:34 +0200

    test: fix coverage config for sonar

--- vite.config.js
+++ vite.config.js
@@ -13,9 +13,7 @@ export default defineConfig({
     environment: 'jsdom',
     setupFiles: ['./src/setupTests.ts'],
     coverage: {
-      provider: 'v8',
-      reporter: ['text', 'json', 'html'],
-      reportsDirectory: 'coverage',
+      reporter: ['text', 'lcov'],
     },
   },
   resolve: {
