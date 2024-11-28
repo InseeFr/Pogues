986166fe96e5798b0d7ca4603d918f47136f7b3a
Quentin Ruhier <quentin.ruhier@insee.fr>
2024-11-28 14:02:36 +0100

    chore: fix eslint config to handle classes correctly

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,12 @@
+import babelParser from '@babel/eslint-parser';
 import js from '@eslint/js';
-import eslintConfigPrettier from 'eslint-config-prettier';
+import react from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import globals from 'globals';
 
 export default [
   js.configs.recommended,
-  eslintConfigPrettier,
   {
     ignores: [
       'dist',
@@ -20,17 +20,28 @@ export default [
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+      sourceType: 'module',
+      parser: babelParser,
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+        requireConfigFile: false,
+        babelOptions: {
+          plugins: ['@babel/plugin-syntax-jsx'], // Add plugin for JSX support
+        },
+      },
     },
     plugins: {
+      react,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
+      'react/jsx-uses-react': 'error',
+      'react/jsx-uses-vars': 'error',
+      'react-refresh/only-export-components': ['off'],
     },
   },
 ];
