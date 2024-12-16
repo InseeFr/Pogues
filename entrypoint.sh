978dd32256a7d7e8ed2adcfcdeb06e0fec9aa711
Chloe Renaud <chloe.renaud@insee.fr>
2024-12-16 13:56:05 +0100

    chore: typescript config

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 echo "window._env_['API_URL'] = '$API_URL';" >> /usr/share/nginx/html/env-config.js
 echo "window._env_['ACTIVE_NAMESPACES'] = '$ACTIVE_NAMESPACES';" >> /usr/share/nginx/html/env-config.js
+echo "window._env_['PUBLIC_ENEMY_URL'] = '$PUBLIC_ENEMY_URL';" >> /usr/share/nginx/html/env-config.js
 echo "window._env_['AUTH_TYPE'] = '$AUTH_TYPE';" >> /usr/share/nginx/html/env-config.js
 echo "window._env_['OIDC_AUTHORITY'] = '$OIDC_AUTHORITY';" >> /usr/share/nginx/html/env-config.js
 echo "window._env_['OIDC_CLIENT_ID'] = '$OIDC_CLIENT_ID';" >> /usr/share/nginx/html/env-config.js
 echo "window._env_['DEFAULT_USER_ID'] = '$DEFAULT_USER_ID';" >> /usr/share/nginx/html/env-config.js
 echo "window._env_['DEFAULT_USER_NAME'] = '$DEFAULT_USER_NAME';" >> /usr/share/nginx/html/env-config.js
 echo "window._env_['DEFAULT_USER_STAMP'] = '$DEFAULT_USER_STAMP';" >> /usr/share/nginx/html/env-config.js
-echo "window._env_['PUBLIC_ENEMY_URL'] = '$PUBLIC_ENEMY_URL';" >> /usr/share/nginx/html/env-config.js
 echo "window._env_['LOG_LEVEL'] = '$LOG_LEVEL';" >> /usr/share/nginx/html/env-config.js
 exec "$@"
--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,6 @@
-import { createRoot } from 'react-dom/client';
+import { StrictMode } from 'react';
+
+import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 
@@ -7,22 +9,21 @@ import configureStore from './store/configure-store';
 import { OidcProvider } from './utils/oidc';
 import { getInit } from './utils/remote-api';
 
-const container = document.getElementById('root');
-
-const renderApp = (Component, init) => {
+const renderApp = (init) => {
   const store = configureStore(init);
-  const root = createRoot(container);
-  root.render(
-    <Provider store={store}>
-      <OidcProvider>
-        <BrowserRouter>
-          <Component />
-        </BrowserRouter>
-      </OidcProvider>
-    </Provider>,
+  ReactDOM.createRoot(document.getElementById('root')).render(
+    <StrictMode>
+      <Provider store={store}>
+        <OidcProvider>
+          <BrowserRouter>
+            <Router />
+          </BrowserRouter>
+        </OidcProvider>
+      </Provider>
+    </StrictMode>,
   );
 };
 
 // Instead of Promise.resolve, deal with a public API endpoint
 
-getInit().then((res) => renderApp(Router, res));
+getInit().then((res) => renderApp(res));
