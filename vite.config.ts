9084a5f735dec90402ed17c44e2883a0cfd8124a
Laurent Caouissin <caouissin.laurent@gmail.com>
2025-01-07 17:22:42 +0100

    test: fix vite config for test

--- vite.config.ts
+++ vite.config.ts
@@ -3,28 +3,38 @@ import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vite.dev/config/
-export default {
-  base: './',
-  plugins: [
-    react(),
-    tsconfigPaths({
-      projects: [
-        './tsconfig.json', // To avoid tsconfigPaths read website tsconfig path
-      ],
-    }),
-    federation({
-      name: '@pogues-legacy',
-      filename: 'remoteEntry.js',
-      exposes: {
-        './App': './src/main.jsx',
-      },
-      shared: ['react/', 'react-dom/'],
-    }),
-  ],
-  build: {
-    target: 'esnext',
-  },
-  define: {
-    global: 'window',
-  },
+const defaultPlugin = [
+  react(),
+  tsconfigPaths({
+    projects: [
+      './tsconfig.json', // To avoid tsconfigPaths read website tsconfig path
+    ],
+  }),
+];
+
+export const buildViteConf = (withFederation: boolean) => {
+  return {
+    base: './',
+    plugins: withFederation
+      ? [
+          ...defaultPlugin,
+          federation({
+            name: '@pogues-legacy',
+            filename: 'remoteEntry.js',
+            exposes: {
+              './App': './src/main.jsx',
+            },
+            shared: ['react/', 'react-dom/'],
+          }),
+        ]
+      : defaultPlugin,
+    build: {
+      target: 'esnext',
+    },
+    define: {
+      global: 'window',
+    },
+  };
 };
+
+export default buildViteConf(true);
