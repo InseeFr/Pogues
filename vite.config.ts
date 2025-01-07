f76d4a283d2dee328aa4749b55a3a170c1f410b6
Laurent Caouissin <caouissin.laurent@gmail.com>
2025-01-07 16:52:27 +0100

    ref(legacy): add federation plugin to expose main

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
+import { federation } from '@module-federation/vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vite.dev/config/
 export default {
+  base: './',
   plugins: [
     react(),
     tsconfigPaths({
@@ -10,7 +12,18 @@ export default {
         './tsconfig.json', // To avoid tsconfigPaths read website tsconfig path
       ],
     }),
+    federation({
+      name: '@pogues-legacy',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './App': './src/main.jsx',
+      },
+      shared: ['react/', 'react-dom/'],
+    }),
   ],
+  build: {
+    target: 'esnext',
+  },
   define: {
     global: 'window',
   },
