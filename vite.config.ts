1a33cc9cf037c1bad17e91c173b276d6259fb42f
Laurent Caouissin <caouissin.laurent@gmail.com>
2025-01-21 17:10:08 +0100

    chore: add federation plugin to legacy

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,40 @@
+import { federation } from '@module-federation/vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vite.dev/config/
-export default {
-  plugins: [
-    react(),
-    tsconfigPaths({
-      projects: [
-        './tsconfig.json', // To avoid tsconfigPaths read website tsconfig path
-      ],
-    }),
-  ],
-  define: {
-    global: 'window',
-  },
+const defaultPlugin = [
+  react(),
+  tsconfigPaths({
+    projects: [
+      './tsconfig.json', // To avoid tsconfigPaths read website tsconfig path
+    ],
+  }),
+];
+
+export const buildViteConf = (withFederation: boolean) => {
+  return {
+    base: './',
+    plugins: withFederation
+      ? [
+          ...defaultPlugin,
+          federation({
+            name: '@pogues-legacy',
+            filename: 'remoteEntry.js',
+            exposes: {
+              './App': './src/main.jsx',
+            },
+            shared: ['react/', 'react-dom/'],
+          }),
+        ]
+      : defaultPlugin,
+    build: {
+      target: 'esnext',
+    },
+    define: {
+      global: 'window',
+    },
+  };
 };
+
+export default buildViteConf(true);
