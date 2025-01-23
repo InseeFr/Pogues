e51f9af89e1cd8b34dd6423f7ed4c0634bcfe1aa
Laurent Caouissin <38245508+laurentC35@users.noreply.github.com>
2025-01-23 14:53:27 +0100

    fix: build of legacy app (absolute url in index.html instead of relative) (#913)

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { federation } from '@module-federation/vite';
 import react from '@vitejs/plugin-react';
+import type { UserConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vite.dev/config/
@@ -12,7 +13,7 @@ const defaultPlugin = [
   }),
 ];
 
-export const buildViteConf = (withFederation: boolean) => {
+export const buildViteConf = (withFederation: boolean): UserConfig => {
   return {
     base: './',
     plugins: withFederation
@@ -33,6 +34,22 @@ export const buildViteConf = (withFederation: boolean) => {
     },
     define: {
       global: 'window',
+    },
+    // https://vite.dev/guide/build.html#advanced-base-options
+    experimental: {
+      renderBuiltUrl(filename, { hostType }) {
+        /**
+         * For html file (only index.html),
+         * We need the urls to be absolute, not relative (fix issue when we load directly /questionnaire/{id}),
+         * because we set baseURl to "./" to make legacy and next work together
+         * But as for the rest, it must remain relative to allow application works in legacy and with the new application.
+         */
+        if (hostType === 'html') {
+          return `/${filename}`;
+        } else {
+          return { relative: true };
+        }
+      },
     },
   };
 };
