c2ab7fc99061b05371dd991b7c4612e6118bb685
Chloe Renaud <renaudchl@gmail.com>
2025-02-13 16:47:42 +0100

    refactor: handle tanstack query in routes loader (#939)

--- next/src/App.tsx
+++ next/src/App.tsx
@@ -11,6 +11,10 @@ const queryClient = new QueryClient();
 // Create a new router instance
 const router = createRouter({
   routeTree,
+  defaultPreload: 'intent',
+  // Since we're using React Query, we don't want loader calls to ever be stale
+  // This will ensure that the loader is always called when the route is preloaded or visited
+  defaultPreloadStaleTime: 0,
   context: { queryClient, user: undefined },
 });
 
--- next/src/api/questionnaires.ts
+++ next/src/api/questionnaires.ts
@@ -1,6 +1,7 @@
+import { queryOptions } from '@tanstack/react-query';
+
 import { CodesList } from '@/models/codesLists';
 import { Questionnaire } from '@/models/questionnaires';
-import { Stamp } from '@/models/stamps';
 
 import { Questionnaire as PoguesQuestionnaire } from './models/pogues';
 import { computeAuthorizationHeader, getBaseURI } from './utils';
@@ -14,9 +15,34 @@ const pathQuestionnaire = 'persistence/questionnaire';
 const pathQuestionnaireList = 'persistence/questionnaires';
 
 /**
+ * Used to retrieve questionnaires associated to a stamp.
+ *
+ * @see {@link getQuestionnaires}
+ */
+export const questionnairesQueryOptions = (stamp: string, token: string) =>
+  queryOptions({
+    queryKey: ['questionnaires', { stamp }],
+    queryFn: () => getQuestionnaires(stamp, token),
+  });
+
+/**
+ * Used to retrieve a questionnaire associated to its id.
+ *
+ * @see {@link getQuestionnaires}
+ */
+export const questionnaireQueryOptions = (
+  questionnaireId: string,
+  token: string,
+) =>
+  queryOptions({
+    queryKey: ['questionnaire', { questionnaireId }],
+    queryFn: () => getQuestionnaire(questionnaireId, token),
+  });
+
+/**
  * Retrieve questionnaires associated to the provided stamp (e.g. "DR59-SNDI59").
  */
-export async function getQuestionnaires(
+async function getQuestionnaires(
   stamp: string,
   token: string,
 ): Promise<Questionnaire[]> {
@@ -34,113 +60,8 @@ export async function getQuestionnaires(
   return res;
 }
 
-/**
- * Retrieve stamps which allow to fetch questionnaires associated to one.
- */
-export async function getStamps(token: string): Promise<Stamp[]> {
-  const url = `${getBaseURI()}/${pathQuestionnaireList}/stamps`;
-  const headers = new Headers();
-  headers.append('Accept', 'application/json');
-  headers.append('Authorization', computeAuthorizationHeader(token));
-
-  const response = await fetch(url, { headers });
-  const json = (await response.json()) as Promise<Stamp[]>;
-  return json;
-}
-
-/**
- * Create new questionnaire
- */
-export async function postQuestionnaire(
-  qr: Questionnaire,
-  stamp: string,
-  token: string,
-) {
-  const url = `${getBaseURI()}/${pathQuestionnaireList}`;
-  const headers = new Headers();
-  headers.append('Content-Type', 'application/json');
-  headers.append('Authorization', computeAuthorizationHeader(token));
-
-  return fetch(url, {
-    method: 'POST',
-    headers,
-    body: JSON.stringify(computePoguesQuestionnaire(qr, stamp)),
-  }).then((res) => {
-    if (res.ok) return res;
-    throw new Error(`Network request failed :${res.statusText}`);
-  });
-}
-
-/** Update the questionnaire of the provided id with a new code lists */
-export async function addQuestionnaireCodesList(
-  questionnaireId: string,
-  codesList: CodesList,
-  token: string,
-) {
-  const questionnaire = await getPoguesQuestionnaire(questionnaireId, token);
-  const codesLists = questionnaire.CodeLists?.CodeList || [];
-  codesLists.push(...computePoguesCodesLists([codesList]));
-  questionnaire.CodeLists = { CodeList: codesLists };
-
-  return putQuestionnaire(questionnaireId, questionnaire, token);
-}
-
-/** Update the questionnaire of the provided id with the new code lists */
-export async function updateQuestionnaireCodesLists(
-  questionnaireId: string,
-  codeLists: CodesList[],
-  token: string,
-) {
-  const questionnaire = await getPoguesQuestionnaire(questionnaireId, token);
-  questionnaire.CodeLists = { CodeList: computePoguesCodesLists(codeLists) };
-
-  return putQuestionnaire(questionnaireId, questionnaire, token);
-}
-
-/**
- * Update questionnaire by id
- */
-export async function putQuestionnaire(
-  id: string,
-  qr: PoguesQuestionnaire,
-  token: string,
-) {
-  const url = `${getBaseURI()}/${pathQuestionnaire}/${id}`;
-  const headers = new Headers();
-  headers.append('Content-Type', 'application/json');
-  headers.append('Authorization', computeAuthorizationHeader(token));
-
-  return fetch(url, {
-    method: 'PUT',
-    headers,
-    body: JSON.stringify(qr),
-  }).then((res) => {
-    if (res.ok) return res;
-    throw new Error(`Network request failed :${res.statusText}`);
-  });
-}
-
-/**
- * Retrieve questionnaire by id with the pogues model
- */
-export async function getPoguesQuestionnaire(
-  id: string,
-  token: string,
-): Promise<PoguesQuestionnaire> {
-  const url = `${getBaseURI()}/${pathQuestionnaire}/${id}`;
-  const headers = new Headers();
-  headers.append('Accept', 'application/json');
-  headers.append('Authorization', computeAuthorizationHeader(token));
-
-  const response = await fetch(url, { headers });
-  const json = (await response.json()) as Promise<PoguesQuestionnaire>;
-  return await json;
-}
-
-/**
- * Retrieve questionnaire by id
- */
-export async function getQuestionnaire(
+/** Retrieve a questionnaire by id. */
+async function getQuestionnaire(
   id: string,
   token: string,
 ): Promise<Questionnaire> {
@@ -156,31 +77,74 @@ export async function getQuestionnaire(
 }
 
 /**
- * Will send a DELETE request in order to remove an existing questionnaire
- *
- * @param {deleteQuestionnaire} id The id of the questionnaire we want to delete
+ * Create a new questionnaire.
  */
-export async function deleteQuestionnaire(id: string, token: string) {
-  const url = `${getBaseURI()}/${pathQuestionnaire}/${id}`;
+export async function postQuestionnaire(
+  qr: Questionnaire,
+  stamp: string,
+  token: string,
+): Promise<Response> {
+  const url = `${getBaseURI()}/${pathQuestionnaireList}`;
   const headers = new Headers();
+  headers.append('Content-Type', 'application/json');
   headers.append('Authorization', computeAuthorizationHeader(token));
 
   return fetch(url, {
-    method: 'DELETE',
+    method: 'POST',
     headers,
+    body: JSON.stringify(computePoguesQuestionnaire(qr, stamp)),
+  }).then((res) => {
+    if (res.ok) return res;
+    throw new Error(`Network request failed: ${res.statusText}`);
   });
 }
 
-export async function getVariablesById(
+/** Update a questionnaire by id. */
+export async function putQuestionnaire(
+  id: string,
+  qr: PoguesQuestionnaire,
+  token: string,
+): Promise<Response> {
+  const url = `${getBaseURI()}/${pathQuestionnaire}/${id}`;
+  const headers = new Headers();
+  headers.append('Content-Type', 'application/json');
+  headers.append('Authorization', computeAuthorizationHeader(token));
+
+  return fetch(url, {
+    method: 'PUT',
+    headers,
+    body: JSON.stringify(qr),
+  }).then((res) => {
+    if (res.ok) return res;
+    throw new Error(`Network request failed :${res.statusText}`);
+  });
+}
+
+/** Update the questionnaire of the provided id with a new codes list. */
+export async function addQuestionnaireCodesList(
+  questionnaireId: string,
+  codesList: CodesList,
+  token: string,
+): Promise<Response> {
+  const questionnaire = await getPoguesQuestionnaire(questionnaireId, token);
+  const codesLists = questionnaire.CodeLists?.CodeList || [];
+  codesLists.push(...computePoguesCodesLists([codesList]));
+  questionnaire.CodeLists = { CodeList: codesLists };
+
+  return putQuestionnaire(questionnaireId, questionnaire, token);
+}
+
+/** Retrieve a questionnaire by id with the pogues model. */
+async function getPoguesQuestionnaire(
   id: string,
   token: string,
-): Promise<unknown> {
-  const url = `${getBaseURI()}/${pathQuestionnaire}/${id}/variables`;
+): Promise<PoguesQuestionnaire> {
+  const url = `${getBaseURI()}/${pathQuestionnaire}/${id}`;
   const headers = new Headers();
   headers.append('Accept', 'application/json');
   headers.append('Authorization', computeAuthorizationHeader(token));
 
-  return fetch(url, {
-    headers,
-  }).then((res) => res.json());
+  const response = await fetch(url, { headers });
+  const json = (await response.json()) as Promise<PoguesQuestionnaire>;
+  return await json;
 }
