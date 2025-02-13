e8aaa19832bd1aa975c60c567b99f94c2dbea69b
Chloe Renaud <renaudchl@gmail.com>
2025-02-13 11:17:19 +0100

    build: (next) oidc spa v6 and set authentification as mandatory (#937)

--- next/src/hooks/useAuth.ts
+++ next/src/hooks/useAuth.ts
@@ -1,38 +1,19 @@
-import { useEffect } from 'react';
-
 import { useOidc } from '@/contexts/oidc';
 
 export type User = {
   givenName?: string;
   familyName?: string;
   stamp?: string;
-  /** Token used for API calls */
-  token: string;
 };
 
 export function useAuth(): User | undefined {
-  //const [user, setUser] = useState<User | undefined>(undefined);
   const oidc = useOidc();
-  console.log(oidc);
-
-  useEffect(() => {
-    // TODO handle log out
-    /*const unsubscribe = auth.onAuthStateChanged((user) => {
-      //setUser(user);
-      router.invalidate();
-    });
-
-    return unsubscribe;*/
-  }, []);
-
-  if (!oidc.isUserLoggedIn) return undefined;
 
   const decodedIdToken = oidc.oidcTokens?.decodedIdToken;
   const user = {
     givenName: decodedIdToken?.given_name as string,
     familyName: decodedIdToken?.family_name as string,
     stamp: decodedIdToken?.timbre as string,
-    token: decodedIdToken?.accessToken as string,
   };
 
   return user;
