9bf840bab8af7874e49b7b5dc95d505a7152b0c9
Quentin Ruhier <quentin.ruhier@insee.fr>
2025-02-21 16:23:27 +0100

    feat: add pogues model download

--- src/api/utils.ts
+++ src/api/utils.ts
@@ -7,3 +7,24 @@ export function getBaseURI(): string {
 export function computeAuthorizationHeader(token: string): string {
   return token ? `Bearer ${token}` : '';
 }
+
+export function downloadAsJson(params: { data: object; filename?: string }) {
+  const { data, filename = 'data.json' } = params;
+  if (!data) {
+    console.error('No data to download.');
+    return;
+  }
+  const jsonData = JSON.stringify(data, null, 2);
+  const blob = new Blob([jsonData], { type: 'application/json' });
+
+  const url = URL.createObjectURL(blob);
+  const a = document.createElement('a');
+  a.href = url;
+  a.download = filename;
+
+  document.body.appendChild(a);
+  a.click();
+
+  document.body.removeChild(a);
+  URL.revokeObjectURL(url);
+}
