6a40a8b206c88f2c2aa2d9082499e3c0d564d005
Laurent Caouissin <caouissin.laurent@gmail.com>
2025-03-17 18:08:24 +0100

    fix(next): remove getEnvVar and use `import.meta.env`

--- next/src/api/instance.ts
+++ next/src/api/instance.ts
@@ -1,13 +1,12 @@
 import axios from 'axios';
 
 import { getOidc } from '@/contexts/oidc';
-import { getEnvVar } from '@/utils/env';
 
 /**
  * Axios instance used to call Pogues API.
  * Automatically set access token and base url.
  */
-export const instance = axios.create({ baseURL: getEnvVar('API_URL') });
+export const instance = axios.create({ baseURL: import.meta.env.VITE_API_URL });
 
 // get access token and add authorization header
 instance.interceptors.request.use(async (config) => {
