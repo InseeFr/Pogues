591d163a4620d4cd6212eb38d0012a0798570593
Chloe Renaud <chloe.renaud@insee.fr>
2025-03-20 17:19:38 +0100

    fix: test env var

--- next/tests/setup.ts
+++ next/tests/setup.ts
@@ -3,6 +3,7 @@ import { cleanup } from '@testing-library/react';
 import { afterEach, vi } from 'vitest';
 
 vi.stubEnv('VITE_API_URL', 'https://mock-api');
+vi.stubEnv('VITE_AUTH_TYPE', 'mock');
 
 afterEach(() => {
   cleanup();
