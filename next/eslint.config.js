d17c6fb85c6a14f356a5f12cd1a217e65969c4ca
Chloe Renaud <chloe.renaud@insee.fr>
2025-03-21 11:19:35 +0100

    ci: removed routeTree.gen.ts from sonar coverage and linter

--- next/eslint.config.js
+++ next/eslint.config.js
@@ -5,7 +5,7 @@ import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
-  { ignores: ['dist', 'src/vite-env.d.ts'] },
+  { ignores: ['dist', 'src/vite-env.d.ts', 'src/routeTree.gen.ts'] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
