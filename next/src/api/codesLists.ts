50326de2d47623a56ac3608744404adc9fe79a2e
Chloe Renaud <chloe.renaud@insee.fr>
2025-03-21 17:21:02 +0100

    feat: handle code list deletion related questions error

--- next/src/api/codesLists.ts
+++ next/src/api/codesLists.ts
@@ -2,6 +2,18 @@ import type { CodesList } from '@/models/codesLists';
 
 import { instance } from './instance';
 
+export enum ERROR_CODES {
+  RELATED_QUESTION_NAMES = 'codelist:relatedquestions:name',
+}
+
+export type CodeListError =
+  | CodeListRelatedQuestionError
+  | { errorCode?: string };
+export type CodeListRelatedQuestionError = {
+  errorCode: ERROR_CODES.RELATED_QUESTION_NAMES;
+  relatedQuestionNames: string[];
+};
+
 /** Create or update a codes list. */
 export async function putCodesList(
   questionnaireId: string,
