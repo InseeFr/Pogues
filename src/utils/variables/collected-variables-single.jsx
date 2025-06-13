4f889ae26a8cb365ecd1fa95d0dd42601b742ded
Chloe Renaud <chloe.renaud@insee.fr>
2025-06-13 10:18:36 +0200

    fix: handle precision variable

--- src/utils/variables/collected-variables-single.jsx
+++ src/utils/variables/collected-variables-single.jsx
@@ -52,54 +52,60 @@ export function getCollectedVariablesSingle(
     );
   }
 
-  // get new clarification variables for codes lists
-  form.CodesList.codes?.forEach((code) => {
-    if (code.precisionid && code.precisionid !== '') {
-      collectedVariables.push(
-        getCollectedVariable(
-          code.precisionid,
-          `${code.precisionid} label`,
-          { z: code.weight, isCollected: '1' },
-          {
-            type: TEXT,
-            codeListReference: undefined,
-            [TEXT]: {
-              maxLength: code.precisionsize,
-            },
-          },
-        ),
-      );
-    }
-  });
+  const noPrecisionVariable = form.visHint === DATATYPE_VIS_HINT.DROPDOWN;
 
-  // get existing clarification variables for codes lists
-  form.CodesList.codes?.forEach((code) => {
-    if (code.precisionByCollectedVariableId) {
-      for (const [variableId, precision] of Object.entries(
-        code.precisionByCollectedVariableId,
-      )) {
-        if (existingVariableIds.has(variableId)) {
-          collectedVariables.push(
-            getCollectedVariable(
-              precision.precisionid,
-              `${precision.precisionid} label`,
-              { z: code.weight, isCollected: '1' },
-              {
-                type: TEXT,
-                codeListReference: undefined,
-                [TEXT]: {
-                  maxLength: precision.precisionsize,
-                },
+  // get new clarification variables for codes lists
+  if (!noPrecisionVariable) {
+    form.CodesList.codes?.forEach((code) => {
+      if (code.precisionid && code.precisionid !== '') {
+        collectedVariables.push(
+          getCollectedVariable(
+            code.precisionid,
+            `${code.precisionid} label`,
+            { z: code.weight, isCollected: '1' },
+            {
+              type: TEXT,
+              codeListReference: undefined,
+              [TEXT]: {
+                maxLength: code.precisionsize,
               },
-              undefined,
-              undefined,
-              variableId,
-            ),
-          );
+            },
+          ),
+        );
+      }
+    });
+  }
+
+  if (!noPrecisionVariable) {
+    // get existing clarification variables for codes lists
+    form.CodesList.codes?.forEach((code) => {
+      if (code.precisionByCollectedVariableId) {
+        for (const [variableId, precision] of Object.entries(
+          code.precisionByCollectedVariableId,
+        )) {
+          if (existingVariableIds.has(variableId)) {
+            collectedVariables.push(
+              getCollectedVariable(
+                precision.precisionid,
+                `${precision.precisionid} label`,
+                { z: code.weight, isCollected: '1' },
+                {
+                  type: TEXT,
+                  codeListReference: undefined,
+                  [TEXT]: {
+                    maxLength: precision.precisionsize,
+                  },
+                },
+                undefined,
+                undefined,
+                variableId,
+              ),
+            );
+          }
         }
       }
-    }
-  });
+    });
+  }
 
   return collectedVariables;
 }
