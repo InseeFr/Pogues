efad29526b34489373a76650841e0cfea0292b5a
Quentin Ruhier <quentin.ruhier@insee.fr>
2025-09-29 09:50:35 +0200

    feat: use the custom dirty state modal in legacy component

--- next/src/components/legacy/index.tsx
+++ next/src/components/legacy/index.tsx
@@ -1,48 +1,25 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-import { Dispatch, SetStateAction, useMemo, useState } from 'react';
+import { useMemo } from 'react';
 
 //@ts-expect-error import jsx component
 import { Main } from '@pogues-legacy/App';
-import { useBlocker } from '@tanstack/react-router';
 import { TFunction } from 'i18next';
 import { ErrorBoundary } from 'react-error-boundary';
 import { useTranslation } from 'react-i18next';
 
+import { useDirtyState } from '@/contexts/DirtyStateContext';
+
 export const LegacyComponent = () => {
   const { t } = useTranslation();
-  const [isDirtyState, setIsDirtyState] = useState<boolean>(false);
+  const { setDirty } = useDirtyState();
 
-  // Block navigation if we are in dirty state
-  useBlocker({
-    // enable blocker only if we are in dirty state
-    enableBeforeUnload: isDirtyState,
-    shouldBlockFn: () => {
-      if (!isDirtyState) return false;
-
-      // Display confirm modal if we are in dirty state
-      const shouldLeave = confirm(t('common.unsavedModification'));
-
-      // The user confirms, so we pursue navigation and reset dirty state
-      if (shouldLeave) {
-        setIsDirtyState(false);
-        return false;
-      }
-
-      // The user cancels, we only block navigation
-      return true;
-    },
-  });
-
-  const myComponent = useMemo(
-    () => legacyApp(setIsDirtyState, t),
-    [setIsDirtyState, t],
-  );
+  const myComponent = useMemo(() => legacyApp(setDirty, t), [setDirty, t]);
 
   return myComponent;
 };
 
 function legacyApp(
-  setIsDirtyState: Dispatch<SetStateAction<boolean>>,
+  setIsDirtyState: (isDirtyState: boolean) => void,
   t: TFunction<'translation', undefined>,
 ) {
   return (
