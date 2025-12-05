d950faccf7bb9ae790a9d3e1362fdcb7b93b5f68
Chloe Renaud <chloe.renaud@insee.fr>
2025-12-05 10:53:44 +0100

    fix: variable name screaming snake case format is no longer mandatory

--- next/src/components/variables/form/utils/name.test.ts
+++ next/src/components/variables/form/utils/name.test.ts
@@ -1,7 +1,7 @@
-import { convertToScreamingCamelCase, screamingCamelCaseRegex } from './name';
+import { convertToScreamingSnakeCase, screamingSnakeCaseRegex } from './name';
 
-it('SCREAMING_CAMEL_CASE regexp is valid', () => {
-  const re = new RegExp(screamingCamelCaseRegex);
+it('SCREAMING_SNAKE_CASE regexp is valid', () => {
+  const re = new RegExp(screamingSnakeCaseRegex);
   // Valid names
   expect(re.exec('VAR')).not.toBeNull();
   expect(re.exec('MY_VAR')).not.toBeNull();
@@ -17,14 +17,14 @@ it('SCREAMING_CAMEL_CASE regexp is valid', () => {
   expect(re.exec('1VAR')).toBeNull();
 });
 
-it('converts to SCREAMING_CAMEL_CASE', () => {
+it('converts to SCREAMING_SNAKE_CASE', () => {
   // Valid values that should not be changed
-  expect(convertToScreamingCamelCase('VAR')).toBe('VAR');
-  expect(convertToScreamingCamelCase('MY_VAR')).toBe('MY_VAR');
-  expect(convertToScreamingCamelCase('MY_VAR1')).toBe('MY_VAR1');
-  expect(convertToScreamingCamelCase('MY_VAR_1')).toBe('MY_VAR_1');
+  expect(convertToScreamingSnakeCase('VAR')).toBe('VAR');
+  expect(convertToScreamingSnakeCase('MY_VAR')).toBe('MY_VAR');
+  expect(convertToScreamingSnakeCase('MY_VAR1')).toBe('MY_VAR1');
+  expect(convertToScreamingSnakeCase('MY_VAR_1')).toBe('MY_VAR_1');
   // Invalid values
-  expect(convertToScreamingCamelCase('var')).toBe('VAR');
-  expect(convertToScreamingCamelCase('my_var')).toBe('MY_VAR');
-  expect(convertToScreamingCamelCase('my-var')).toBe('MY_VAR');
+  expect(convertToScreamingSnakeCase('var')).toBe('VAR');
+  expect(convertToScreamingSnakeCase('my_var')).toBe('MY_VAR');
+  expect(convertToScreamingSnakeCase('my-var')).toBe('MY_VAR');
 });
