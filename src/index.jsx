7e69e3574786c785a978dcc1ba898323009283e0
Laurent Caouissin <laurent.caouissin@insee.fr>
2026-03-10 17:23:37 +0100

    chore(legacy): remove StrictMode because not working

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,8 @@
-import { StrictMode } from 'react';
-
 import ReactDOM from 'react-dom/client';
 
 import { Main } from './main';
 import { getAccessToken } from './utils/oidc';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <Main getAccessToken={getAccessToken} />
-  </StrictMode>,
+  <Main getAccessToken={getAccessToken} />,
 );
