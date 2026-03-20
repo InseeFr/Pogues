afffcef6af2b3bb0423e7ddc1df4401f8bb8677c
Laurent Caouissin <laurent.caouissin@insee.fr>
2026-03-20 10:24:06 +0100

    chore: remove very deprecated e2e test

--- e2e/po/edit-question.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  CancelButton: '[type=reset]',
-  Tabs: '.tabs',
-  DeclarationTab: '.tabs li:nth-child(2)',
-  DeclarationContent: '.nav-content:nth-child(3)',
-  DeclarationList: '.list-entry-form_list li',
-  DeclarationTextArea: '.list-entry-form_list li',
-  DeclarationAdd: '.list-entry-form_list li:last-child',
-};
