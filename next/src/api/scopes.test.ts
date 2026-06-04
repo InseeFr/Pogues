e7f56ac65977da64bce04576b0042769d9d9acb6
Laurent Caouissin <laurent.caouissin@insee.fr>
2026-06-04 10:55:33 +0200

    fix: scope test

--- next/src/api/scopes.test.ts
+++ next/src/api/scopes.test.ts
@@ -2,22 +2,24 @@ import nock from 'nock'
 
 import type { Scopes } from '@/models/scopes'
 
+import { VariableScope } from './models/variableDTO'
 import { getScopes } from './scopes'
 
 vi.mock('@/lib/auth/oidc')
 
-const questionnaireScope: Scopes = new Map<string, string>([
-  ['id1', 'scope1'],
-  ['id2', 'scope2'],
-])
+const questionnaireScopeApi: VariableScope[] = [
+  { id: 'id1', label: 'scope1' },
+  { id: 'id2', label: 'scope2' },
+]
 
-// Convert Map to plain object for JSON serialization
-const questionnaireScopeObject = Object.fromEntries(questionnaireScope)
+const questionnaireScope: Scopes = new Map<string, string>(
+  questionnaireScopeApi.map((scope) => [scope.id, scope.label]),
+)
 
 it('Get scopes works', async () => {
   nock('https://mock-api')
     .get('/persistence/questionnaire/my-questionnaire-id/variables-scopes')
-    .reply(200, questionnaireScopeObject)
+    .reply(200, questionnaireScopeApi)
 
   const res = await getScopes('my-questionnaire-id')
   expect(res).toEqual(questionnaireScope)
