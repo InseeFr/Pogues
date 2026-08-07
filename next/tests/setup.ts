1e2bb3845cfc46e8a58a1c7420fe4eeca347f40e
Mailine Nguyen <64129348+MailineN@users.noreply.github.com>
2026-08-07 11:50:39 +0200

    chore(deps): migrate to node 24 (#1197)
    * chore(deps): migrate to node 24
    
    * fix: set lts version in nvmrc
    
    * fix: prepare script

--- next/tests/setup.ts
+++ next/tests/setup.ts
@@ -3,7 +3,8 @@ import { cleanup } from '@testing-library/react'
 import { afterEach } from 'vitest'
 
 if (typeof PointerEvent === 'undefined') {
-  globalThis.PointerEvent = class PointerEvent extends MouseEvent {} as unknown as typeof PointerEvent
+  globalThis.PointerEvent =
+    class PointerEvent extends MouseEvent {} as unknown as typeof PointerEvent
 }
 
 afterEach(() => {
